3bb342e200ccb08d95a793f3f22d3014efe7ae09
Kamil Piechaczek <pomek@pomek.pl>
2016-12-02 10:21:20 +0100

    Introduced new binary script for Travis.
    It allows keeping configuration for Travis in single file.

---
+++ packages/ckeditor5-dev-tests/bin/test-travis.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+node_modules/.bin/ckeditor5-dev-tests --coverage --ignore-duplicates --reporter=dots && \
+	sed -i.backup 's/build\/\.automated-tests\/ckeditor5\/[^\/]*/src/g' build/.automated-tests/coverage/lcov.info
