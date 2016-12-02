db41f7899d8fd23221cb2e731d2be8a03196bab3
Kamil Piechaczek <pomek@pomek.pl>
2016-12-02 13:08:00 +0100

    Linter will be running on the CI.

--- packages/ckeditor5-dev-tests/bin/test-travis.sh
+++ packages/ckeditor5-dev-tests/bin/test-travis.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+node_modules/.bin/gulp lint && \
 node_modules/.bin/ckeditor5-dev-tests --coverage --ignore-duplicates --reporter=dots && \
-	sed -i.backup 's/build\/\.automated-tests\/ckeditor5\/[^\/]*/src/g' build/.automated-tests/coverage/lcov.info
+sed -i.backup 's/build\/\.automated-tests\/ckeditor5\/[^\/]*/src/g' build/.automated-tests/coverage/lcov.info
