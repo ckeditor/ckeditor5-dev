7e3710239c5382bcdd0c6a6f4a61c6d185418d1b
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-12-30 18:14:57 +0100

    Simplified dev-docs so the task only build API docs.

--- packages/ckeditor5-dev-docs/lib/index.js
+++ packages/ckeditor5-dev-docs/lib/index.js
@@ -5,7 +5,30 @@
 
 'use strict';
 
+const buildApiDocs = require( 'docs-builder/src/tasks/build-api-docs' );
+const { tools } = require( '@ckeditor/ckeditor5-dev-utils' );
+
 module.exports = {
-	docs: require( './docs' ),
-	utils: require( './utils' )
+	build
 };
+
+/**
+ * Builds CKEditor 5 documentation.
+ *
+ * @param {Object} config
+ * @param {Array.<String>} config.sourceFiles Glob pattern with source files.
+ * @param {String} config.readmePath Path to `README.md`.
+ * @param {String} config.destinationPath Path under which documentation should be generated.
+ * @returns {Promise}
+ */
+function build( config ) {
+	return tools.clean( config.destinationPath, '.' )
+		.then( () => {
+			const sourceFiles = [
+				config.readmePath,
+				...config.sourceFiles
+			];
+
+			return buildApiDocs( sourceFiles, config.destinationPath, [], [] );
+		} );
+}
