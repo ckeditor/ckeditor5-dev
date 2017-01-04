58a08df56f2d47e4e6525a9a8e5db214982ade58
Maciej Bukowski <ma2ciek@gmail.com>
2017-01-04 11:14:26 +0100

    Added missing dep. Added more tests for karma config.

--- packages/ckeditor5-dev-tests/tests/utils/getkarmaconfig.js
+++ packages/ckeditor5-dev-tests/tests/utils/getkarmaconfig.js
@@ -5,6 +5,7 @@
 const mockery = require( 'mockery' );
 const { expect } = require( 'chai' );
 const sinon = require( 'sinon' );
+const path = require( 'path' );
 
 describe( 'getkarmaconfig', () => {
 	let getKarmaConfig;
@@ -13,12 +14,13 @@ describe( 'getkarmaconfig', () => {
 	beforeEach( () => {
 		sandbox = sinon.sandbox.create();
 		sandbox.stub( process, 'cwd', () => 'workspace' );
+		sandbox.stub( path, 'join', ( ...chunks ) => chunks.join( '/' ) );
 
 		mockery.enable( {
 			warnOnReplace: false,
 			warnOnUnregistered: false
 		} );
-		mockery.registerMock( 'getWebpackConfigForAutomatedTests', ( options ) => options );
+		mockery.registerMock( './getwebpackconfigforautomatedtests', ( options ) => options );
 
 		getKarmaConfig = require( '../../lib/utils/getkarmaconfig' );
 	} );
@@ -28,13 +30,111 @@ describe( 'getkarmaconfig', () => {
 		mockery.disable();
 	} );
 
-	it( 'should return basic karma config', () => {
+	it( 'should return basic karma config for all tested files', () => {
 		const karmaConfig = getKarmaConfig( {
 			files: [ '*' ],
+			reporter: 'mocha',
+			sourceMap: false,
+			coverage: false,
+			browsers: 'Chrome',
+			watch: false,
+			verbose: false
+		} );
+
+		const expectedFile = 'workspace/node_modules/ckeditor5-!(dev)*/tests/**/*.js';
+
+		expect( karmaConfig ).to.deep.eq( {
+			basePath: 'workspace',
+			frameworks: [ 'mocha', 'chai', 'sinon' ],
+			files: [ expectedFile ],
+			exclude: [
+				path.join( '**', 'tests', '**', '_utils', '**', '*.js' ),
+				path.join( '**', 'tests', '**', 'manual', '**', '*.js' )
+			],
+			preprocessors: {
+				[ expectedFile ]: [ 'webpack' ]
+			},
+			webpack: {
+				files: [ expectedFile ],
+				sourceMap: false,
+				coverage: false,
+			},
+			webpackMiddleware: {
+				noInfo: true,
+				stats: {
+					chunks: false
+				}
+			},
+			reporters: [ 'mocha' ],
+			port: 9876,
+			colors: true,
+			logLevel: 'INFO',
+			browsers: 'Chrome',
+			customLaunchers: {
+				CHROME_TRAVIS_CI: {
+					base: 'Chrome',
+					flags: [ '--no-sandbox' ]
+				}
+			},
+			singleRun: true,
+			concurrency: Infinity,
+			browserNoActivityTimeout: 0
+		} );
+	} );
+
+	it( 'should return karma config with current package\'s tests', () => {
+		const karmaConfig = getKarmaConfig( {
+			files: [ '/' ],
 			reporter: 'mocha'
 		} );
 
-		expect( karmaConfig.reporters ).to.deep.equal( [ 'mocha' ] );
-		expect( karmaConfig.files ).to.deep.eq( [ 'workspace/node_modules/ckeditor5-!(dev)*/tests/**/*.js' ] );
+		expect( karmaConfig.files ).to.deep.eq( [
+			'workspace/tests/**/*.js',
+		] );
+	} );
+
+	it( 'should return karma config with transformed given package files', () => {
+		const karmaConfig = getKarmaConfig( {
+			files: [ 'engine', 'utils' ],
+			reporter: 'mocha'
+		} );
+
+		expect( karmaConfig.files ).to.deep.eq( [
+			'workspace/node_modules/ckeditor5-engine/tests/**/*.js',
+			'workspace/node_modules/ckeditor5-utils/tests/**/*.js',
+		] );
+	} );
+
+	it( 'should return karma config with transformed excluded packages as files', () => {
+		const karmaConfig = getKarmaConfig( {
+			files: [ '!engine' ],
+			reporter: 'mocha'
+		} );
+
+		expect( karmaConfig.files ).to.deep.eq( [
+			'workspace/node_modules/ckeditor5-!(engine)*/tests/**/*.js',
+		] );
+	} );
+
+	it( 'should return karma config with transformed package directories as files', () => {
+		const karmaConfig = getKarmaConfig( {
+			files: [ 'engine/view' ],
+			reporter: 'mocha'
+		} );
+
+		expect( karmaConfig.files ).to.deep.eq( [
+			'workspace/node_modules/ckeditor5-engine/tests/view/**/*.js',
+		] );
+	} );
+
+	it( 'should return karma config with transformed glob as files', () => {
+		const karmaConfig = getKarmaConfig( {
+			files: [ 'engine/model/**/*.js' ],
+			reporter: 'mocha'
+		} );
+
+		expect( karmaConfig.files ).to.deep.eq( [
+			'workspace/node_modules/ckeditor5-engine/tests/model/**/*.js',
+		] );
 	} );
 } );
