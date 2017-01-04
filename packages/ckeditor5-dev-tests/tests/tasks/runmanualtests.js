a5aff04fb22b1c3b5ddeff556c0723aa35aad1f5
Maciej Bukowski <ma2ciek@gmail.com>
2017-01-04 17:38:42 +0100

    Refactored test for runManualTest.

--- packages/ckeditor5-dev-tests/tests/tasks/runmanualtests.js
+++ packages/ckeditor5-dev-tests/tests/tasks/runmanualtests.js
@@ -3,7 +3,6 @@
 'use strict';
 
 const mockery = require( 'mockery' );
-const { expect } = require( 'chai' );
 const sinon = require( 'sinon' );
 const path = require( 'path' );
 
@@ -24,31 +23,23 @@ describe( 'runManualTests', () => {
 	} );
 
 	it( 'should run manual tests and return promise', () => {
-		mockery.registerMock( '../utils/createmanualtestserver', ( buildDir ) => {
-			expect( buildDir ).to.equal( 'workspace/build/.manual-tests' );
+		const serverSpy = sandbox.spy( () => Promise.resolve() );
+		const htmlFileCompilerSpy = sandbox.spy( () => Promise.resolve() );
+		const scriptCompiler = sandbox.spy( () => Promise.resolve() );
 
-			return Promise.resolve();
-		} );
-
-		mockery.registerMock( '../utils/compilemanualtestscripts', ( buildDir, manualTestPattern ) => {
-			expect( buildDir ).to.equal( 'workspace/build/.manual-tests' );
-			expect( manualTestPattern ).to.equal( 'workspace/node_modules/ckeditor5-*/tests/**/manual/**' );
-
-			return Promise.resolve();
-		} );
-		mockery.registerMock( '../utils/compilemanualtesthtmlfiles', ( buildDir, manualTestPattern ) => {
-			expect( buildDir ).to.equal( 'workspace/build/.manual-tests' );
-			expect( manualTestPattern ).to.equal( 'workspace/node_modules/ckeditor5-*/tests/**/manual/**' );
-
-			return Promise.resolve();
-		} );
+		mockery.registerMock( '../utils/createmanualtestserver', serverSpy );
+		mockery.registerMock( '../utils/compilemanualtesthtmlfiles', htmlFileCompilerSpy );
+		mockery.registerMock( '../utils/compilemanualtestscripts', scriptCompiler );
 
 		sandbox.stub( path, 'join', ( ...chunks ) => chunks.join( '/' ) );
 		sandbox.stub( process, 'cwd', () => 'workspace' );
 
 		const runManualTests = require( '../../lib/tasks/runManualTests' );
-		const promise = runManualTests();
 
-		return promise;
+		return runManualTests().then( () => {
+			sinon.assert.calledWith( serverSpy, 'workspace/build/.manual-tests' );
+			sinon.assert.calledWith( htmlFileCompilerSpy, 'workspace/build/.manual-tests', 'workspace/node_modules/ckeditor5-*/tests/**/manual/**' );
+			sinon.assert.calledWith( scriptCompiler, 'workspace/build/.manual-tests', 'workspace/node_modules/ckeditor5-*/tests/**/manual/**' );
+		} );
 	} );
 } );
