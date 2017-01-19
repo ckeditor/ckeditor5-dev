d125f64a6b6f389c5dca4b3cba512183e9a16892
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-01-19 10:42:36 +0100

    There's no need to sort anything here.

--- packages/ckeditor5-dev-tests/bin/create-mgit-json.js
+++ packages/ckeditor5-dev-tests/bin/create-mgit-json.js
@@ -14,7 +14,7 @@ const cwd = process.cwd();
 const packageJson = require( path.join( cwd, 'package.json' ) );
 const mgitJson = path.resolve( cwd, 'mgit.json' );
 
-let json = {
+const json = {
 	'packages': 'packages/',
 	'dependencies': {}
 };
@@ -42,17 +42,5 @@ if ( deps.length ) {
 		json.dependencies[ item.replace( /@ckeditor\//, '' ) ] = item.slice( 1 ) + `#ckeditor5/t/389`;
 	} );
 
-	json.dependencies = sortObject( json.dependencies );
-
 	fs.writeFileSync( mgitJson, JSON.stringify( json, null, 2 ) + '\n', 'utf-8' );
-}
-
-function sortObject( obj ) {
-	Object.keys( obj ).sort().forEach( key => {
-		const val = obj[ key ];
-		delete obj[ key ];
-		obj[ key ] = val;
-	} );
-
-	return obj;
 }
