82c062d6a8d5493985840728ddefa8586694c30d
Piotrek Koszuliński <pkoszulinski@gmail.com>
2017-01-31 12:11:53 +0100

    Fixed: The line was in a wrong file ;<.

--- packages/ckeditor5-dev-tests/bin/test-travis.sh
+++ packages/ckeditor5-dev-tests/bin/test-travis.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-npm install gulp mgit2 lerna@2.0.0-beta.34 codeclimate-test-reporter @ckeditor/ckeditor5-dev-lint && \
 node_modules/.bin/gulp lint && \
 node_modules/.bin/ckeditor5-dev-tests --coverage --reporter=dots
