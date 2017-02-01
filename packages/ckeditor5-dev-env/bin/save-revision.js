49447f86dd37720c31ef510831bf6748fe68eea2
Kamil Piechaczek <pomek@pomek.pl>
2017-02-01 08:22:16 +0100

    Fix: Script for saving revisions will use "mgit save-hashes".

--- packages/ckeditor5-dev-env/bin/save-revision.js
+++ packages/ckeditor5-dev-env/bin/save-revision.js
@@ -15,7 +15,6 @@ if ( branch !== 'master' ) {
 
 const path = require( 'path' );
 const { tools } = require( '@ckeditor/ckeditor5-dev-utils' );
-const executeOnDependencies = require( '@ckeditor/ckeditor5-dev-env/lib/release-tools/utils/executeondependencies' );
 
 const mainBranch = `${ branch }-revisions`;
 
@@ -24,20 +23,10 @@ const mainBranch = `${ branch }-revisions`;
 exec( `git clone -b ${ mainBranch } https://${ process.env.GITHUB_USER }:${ process.env.GITHUB_PASSWORD }@github.com/ckeditor/ckeditor5.git` );
 // jscs:enable maximumLineLength
 
-const cwd = path.join( process.cwd(), 'ckeditor5' );
-const mgitJsonPath = path.join( cwd, 'mgit.json' );
-const mgitJson = require( mgitJsonPath );
-const depsHashes = {};
-
-const execDepsOptions = {
-	cwd,
-	workspace: mgitJson.packages
-};
-
 // Change current dir to cloned repository.
-process.chdir( cwd );
+process.chdir( path.join( process.cwd(), 'ckeditor5' ) );
 
-// Install Mgit
+// Install Mgit.
 exec( 'npm install -g mgit2' );
 
 // Install dependencies.
@@ -48,44 +37,17 @@ exec( 'mgit bootstrap --recursive --resolver-url-template="https://github.com/\\
 // Checkout out each package to master.
 exec( `mgit exec "git checkout ${ branch }"` );
 
-// Get hashes from all dependencies
-executeOnDependencies( execDepsOptions, functionToExecute )
-	.then( () => {
-		tools.updateJSONFile( mgitJsonPath, ( json ) => {
-			for ( const packageName of Object.keys( depsHashes ) ) {
-				let packageRepo = packageName;
+// Save hashes from all dependencies.
+exec( 'mgit save-hashes' );
 
-				if ( packageRepo.startsWith( '@' ) ) {
-					packageRepo = packageName.slice( 1 );
-				}
+const commitMessage = `[${ process.env.TRAVIS_REPO_SLUG }] Updated hashes.`;
 
-				json.dependencies[ packageName ] = packageRepo + '#' + depsHashes[ packageName ];
-			}
-
-			return json;
-		} );
-
-		const commitMessage = `[${ process.env.TRAVIS_REPO_SLUG }] Updated hashes.`;
-
-		if ( exec( 'git diff --name-only' ).trim().length ) {
-			exec( `git add mgit.json && git commit -m "${ commitMessage } "` );
-			exec( `git push origin ${ mainBranch }` );
-		}
-	} )
-	.catch( ( err ) => {
-		process.exitCode = 1;
-
-		console.log( err );
-	} );
+// Check whether the mgit.json has changed.
+if ( exec( 'git diff --name-only mgit.json' ).trim().length ) {
+	exec( `git add mgit.json && git commit -m "${ commitMessage } "` );
+	exec( `git push origin ${ mainBranch }` );
+}
 
 function exec( command ) {
 	return tools.shExec( command, { verbosity: 'error' } );
-}
-
-function functionToExecute( dependencyName, dependencyPath ) {
-	process.chdir( dependencyPath );
-
-	depsHashes[ dependencyName ] = exec( 'git rev-parse HEAD' ).trim();
-
-	return Promise.resolve();
 }
