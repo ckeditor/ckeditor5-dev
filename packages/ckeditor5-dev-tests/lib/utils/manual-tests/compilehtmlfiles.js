3521e8d19df0d6bbbf63ede9b94e5ac10c952e90
Maciej Bukowski <ma2ciek@gmail.com>
2017-02-09 16:29:08 +0100

    Fixed: Fixed minor issues and code style in manual tests build process.

--- packages/ckeditor5-dev-tests/lib/utils/manual-tests/compilehtmlfiles.js
+++ packages/ckeditor5-dev-tests/lib/utils/manual-tests/compilehtmlfiles.js
@@ -15,6 +15,8 @@ const gutil = require( 'gulp-util' );
 const commonmark = require( 'commonmark' );
 const combine = require( 'dom-combiner' );
 const getRelativeFilePath = require( '../getrelativefilepath' );
+const _ = require( 'lodash' );
+const chokidar = require( 'chokidar' );
 
 const reader = new commonmark.Parser();
 const writer = new commonmark.HtmlRenderer();
@@ -22,56 +24,58 @@ const writer = new commonmark.HtmlRenderer();
 const viewTemplate = fs.readFileSync( path.join( __dirname, 'template.html' ), 'utf-8' );
 
 module.exports = function compileManualTestHtmlFiles( buildDir, manualTestPattern ) {
-	const sourceMdFiles = globSync( path.join( manualTestPattern, '*.md' ) );
-	const sourceHtmlFiles = sourceMdFiles.map( ( mdFile ) => setExtension( mdFile, 'html' ) );
-	const sourceDirs = uniq( sourceMdFiles.map( file => path.dirname( file ) ) );
-	const sourceFilePathBases = sourceMdFiles.map( ( mdFile ) => getFilePathWithoutExtension( mdFile ) );
+	const sourceMDFiles = globSync( path.join( manualTestPattern, '*.md' ) );
+	const sourceHtmlFiles = sourceMDFiles.map( ( mdFile ) => setExtension( mdFile, 'html' ) );
+	const sourceDirs = uniq( sourceMDFiles.map( file => path.dirname( file ) ) );
+	const sourceFilePathBases = sourceMDFiles.map( ( mdFile ) => getFilePathWithoutExtension( mdFile ) );
+	const staticFiles = _.flatten( sourceDirs.map( sourceDir => {
+		return globSync( path.join( sourceDir, '**', '*.!(js|html|md)' ) );
+	} ) );
 
 	fs.ensureDirSync( buildDir );
 
 	// Copy all files which can be found in the directories with manual tests
 	// to the build dir.
-	sourceDirs.forEach( sourceDir => copyStaticFiles( buildDir, sourceDir ) );
+	staticFiles.forEach( staticFile => copyStaticFile( buildDir, staticFile ) );
 
 	// Generate real HTML files out of the MD + HTML files of each test.
 	sourceFilePathBases.forEach( sourceFilePathBase => compileTestHtmlFile( buildDir, sourceFilePathBase ) );
 
-	// Watch file and compile on change.
-	watchFiles( sourceMdFiles, ( mdFile ) => compileTestHtmlFile( buildDir, getFilePathWithoutExtension( mdFile ) ) );
-	watchFiles( sourceHtmlFiles, ( mdFile ) => compileTestHtmlFile( buildDir, getFilePathWithoutExtension( mdFile ) ) );
+	// Watch files and compile on change.
+	watchFiles( [ ...sourceMDFiles, ...sourceHtmlFiles ], ( file ) => {
+		compileTestHtmlFile( buildDir, getFilePathWithoutExtension( file ) );
+	} );
 };
 
 function compileTestHtmlFile( buildDir, sourceFilePathBase ) {
 	const log = logger();
-	const sourceMdFilePath = setExtension( sourceFilePathBase, 'md' );
+	const sourceMDFilePath = setExtension( sourceFilePathBase, 'md' );
 	const sourceHtmlFilePath = setExtension( sourceFilePathBase, 'html' );
-	const sourceJsFilePath = setExtension( sourceFilePathBase, 'js' );
+	const sourceJSFilePath = setExtension( sourceFilePathBase, 'js' );
 
-	const relativeHtmlFilePath = getRelativeFilePath( sourceHtmlFilePath );
-	const relativeJsFilePath = getRelativeFilePath( sourceJsFilePath );
-
-	console.log( relativeJsFilePath );
+	const absoluteHtmlFilePath = getRelativeFilePath( sourceHtmlFilePath );
+	const absoluteJsFilePath = getRelativeFilePath( sourceJSFilePath );
 
 	log.info( `Processing '${ gutil.colors.cyan( sourceFilePathBase ) }'...` );
 
 	// Compile test instruction (Markdown file).
-	const parsedMarkdownTree = reader.parse( fs.readFileSync( sourceMdFilePath, 'utf-8' ) );
+	const parsedMarkdownTree = reader.parse( fs.readFileSync( sourceMDFilePath, 'utf-8' ) );
 	const manualTestInstructions = '<div class="manual-test-sidebar">' + writer.render( parsedMarkdownTree ) + '</div>';
 
 	// Load test view (HTML file).
-	const htmlView = fs.readFileSync( `${ sourceHtmlFilePath }`, 'utf-8' );
+	const htmlView = fs.readFileSync( sourceHtmlFilePath, 'utf-8' );
 
 	// Attach script file to the view.
 	const scriptTag =
 		'<body class="manual-test-container">' +
-			`<script src="/${ relativeJsFilePath }"></script>` +
+			`<script src="/${ absoluteJsFilePath }"></script>` +
 		'</body>';
 
 	// Concat the all HTML parts to single one.
 	const preparedHtml = combine( viewTemplate, manualTestInstructions, htmlView, scriptTag );
 
 	// Prepare output path.
-	const outputFilePath = path.join( buildDir, relativeHtmlFilePath );
+	const outputFilePath = path.join( buildDir, absoluteHtmlFilePath );
 
 	fs.outputFileSync( outputFilePath, preparedHtml );
 
@@ -79,13 +83,9 @@ function compileTestHtmlFile( buildDir, sourceFilePathBase ) {
 }
 
 // Copies all non JS/HTML/MD files to build dir. Their relative paths to JS/HTML files are maintained.
-function copyStaticFiles( buildDir, sourceDir ) {
-	const files = globSync( path.join( sourceDir, '**', '*.!(js|html|md)' ) );
-
-	for ( const file of files ) {
-		const outputFilePath = path.join( buildDir, getRelativeFilePath( file ) );
-		fs.copySync( file, outputFilePath );
-	}
+function copyStaticFile( buildDir, staticFile ) {
+	const outputFilePath = path.join( buildDir, getRelativeFilePath( staticFile ) );
+	fs.copySync( staticFile, outputFilePath );
 }
 
 function uniq( arr ) {
@@ -106,27 +106,7 @@ function getFilePathWithoutExtension( file ) {
 
 function watchFiles( filePaths, onChange ) {
 	for ( const filePath of filePaths ) {
-		watchFile( filePath, onChange );
+		const debouncedOnChange = _.debounce( () => onChange( filePath ), 500 );
+		chokidar.watch( filePath ).on( 'all', debouncedOnChange );
 	}
-}
-
-function watchFile( filePath, onChange ) {
-	const debouncedOnChange = debounce( () => onChange( filePath ), 500 );
-	fs.watch( filePath, debouncedOnChange );
-}
-
-function debounce( callback, delay ) {
-	let timerId;
-
-	return function( ...args ) {
-		if ( timerId ) {
-			clearTimeout( timerId );
-		}
-
-		timerId = setTimeout( () => {
-			timerId = null;
-
-			callback.apply( null, args );
-		}, delay );
-	};
 }
