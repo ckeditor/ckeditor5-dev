beab0c9b619d5bd44d539f593b6c85fdc8d58301
Kamil Piechaczek <pomek@pomek.pl>
2017-03-10 13:17:17 +0100

    Internal: "mgit.json" will be always taken from the main branch.

--- packages/ckeditor5-dev-tests/bin/save-revision.js
+++ packages/ckeditor5-dev-tests/bin/save-revision.js
@@ -17,11 +17,11 @@ if ( branch !== 'master' ) {
 const path = require( 'path' );
 const { tools } = require( '@ckeditor/ckeditor5-dev-utils' );
 
-const mainBranch = `${ branch }-revisions`;
+const revisionBranch = `${ branch }-revisions`;
 
 // Clone the repository.
 exec(
-	`git clone -b ${ mainBranch } ` +
+	`git clone -b ${ revisionBranch } ` +
 	`https://${ process.env.GITHUB_USER }:${ process.env.GITHUB_PASSWORD }@github.com/ckeditor/ckeditor5.git`
 );
 
@@ -31,11 +31,11 @@ process.chdir( path.join( process.cwd(), 'ckeditor5' ) );
 // Install Mgit.
 exec( 'npm install -g mgit2' );
 
+// Get the latest `mgit.json`.
+exec( `git checkout origin/${ branch } mgit.json` );
+
 // Install dependencies.
 exec( 'mgit bootstrap --recursive --resolver-url-template="https://github.com/\\\${ path }.git"' );
-
-// Checkout out each package to master.
-exec( `mgit exec "git checkout ${ branch }"` );
 
 // Save hashes from all dependencies.
 exec( 'mgit save-hashes' );
@@ -45,7 +45,7 @@ const commitMessage = `[${ process.env.TRAVIS_REPO_SLUG }] Updated hashes.`;
 // Check whether the mgit.json has changed. It might not have changed if, e.g., a build was restarted.
 if ( exec( 'git diff --name-only mgit.json' ).trim().length ) {
 	exec( `git add mgit.json && git commit -m "${ commitMessage }"` );
-	exec( `git push origin ${ mainBranch }` );
+	exec( `git push origin ${ revisionBranch }` );
 }
 
 function exec( command ) {
