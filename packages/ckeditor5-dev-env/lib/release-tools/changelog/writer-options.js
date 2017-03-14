581ac652219372c1a254fc5da7c9cda146f2f16f
Kamil Piechaczek <pomek@pomek.pl>
2017-03-14 18:31:18 +0100

    Fix: In some cases additional description in changelog was not added properly.

--- packages/ckeditor5-dev-env/lib/release-tools/changelog/writer-options.js
+++ packages/ckeditor5-dev-env/lib/release-tools/changelog/writer-options.js
@@ -62,11 +62,14 @@ module.exports = {
 function transformCommit( commit, displayLog = true ) {
 	const log = logger( displayLog ? 'info' : 'error' );
 
+	// For merge commit from Github, additional description is provided as "footer".
+	if ( !commit.body && commit.footer ) {
+		commit.body = commit.footer;
+		commit.footer = null;
+	}
+
 	if ( commit.header.startsWith( 'Merge' ) ) {
-		// For merge commit from Github, additional description is provided as "footer".
-		// For manually merge commit, additional description is provides as "body".
-		const descriptionKey = commit.body ? 'body' : 'footer';
-		const parsedHeader = parserOptions.headerPattern.exec( commit[ descriptionKey ] );
+		const parsedHeader = parserOptions.headerPattern.exec( commit.body );
 
 		if ( parsedHeader ) {
 			parserOptions.headerCorrespondence.forEach( ( key, index ) => {
@@ -75,7 +78,7 @@ function transformCommit( commit, displayLog = true ) {
 
 			// Remove the new header from commit body in order to avoid
 			// duplicating the same sentence in a changelog description.
-			commit[ descriptionKey ] = commit[ descriptionKey ] .replace( parserOptions.headerPattern, '' ).trim();
+			commit.body = commit.body.replace( parserOptions.headerPattern, '' ).trim();
 		}
 	}
 
@@ -96,7 +99,7 @@ function transformCommit( commit, displayLog = true ) {
 		logMessage += chalk.red( 'INVALID' );
 	}
 
-	if ( commit.header.startsWith( 'Merge' ) ) {
+	if ( commit.header.startsWith( 'Merge' ) && hasCorrectType ) {
 		const indentSize = '[XX:YY:ZZ] * 1234567 '.length;
 		logMessage += `\n${ ' '.repeat( indentSize ) }"${ commit.type }: ${ commit.subject }"`;
 	}
