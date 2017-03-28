c19ac1f20249855affe841e487b4c16379071ade
Maciej Bukowski <ma2ciek@gmail.com>
2017-03-28 19:28:13 +0200

    Fixed translations upload script.

--- packages/ckeditor5-dev-env/tests/translations/upload.js
+++ packages/ckeditor5-dev-env/tests/translations/upload.js
@@ -10,57 +10,72 @@
 const path = require( 'path' );
 const sinon = require( 'sinon' );
 const mockery = require( 'mockery' );
-const transifexService = require( '../../lib/translations/transifex-service' );
-const fs = require( 'fs' );
 
 describe( 'upload', () => {
-	let sandbox;
+	const sandbox = sinon.sandbox.create();
 	let upload;
+	let packageNames;
+	let stubs;
 
 	beforeEach( () => {
-		sandbox = sinon.sandbox.create();
-		mockery.registerMock( '@ckeditor/ckeditor5-dev-utils', {
-			logger: () => ( {
-				info() {},
-				error() {}
-			} )
+		mockery.enable( {
+			useCleanCache: true,
+			warnOnReplace: true,
+			warnOnUnregistered: false,
 		} );
+
+		stubs = {
+			logger: {
+				info: sandbox.stub(),
+				warning: sandbox.stub(),
+				error: err => {
+					throw err;
+				},
+			},
+
+			transifexService: {
+				getResources: sandbox.spy( () => Promise.resolve( [ {
+					slug: 'ckeditor5-core'
+				} ] ) ),
+				postResource: sandbox.spy( () => Promise.resolve( '[]' ) ),
+				putResourceContent: sandbox.spy( () => Promise.resolve( '{}' ) ),
+			},
+
+			fs: {
+				readdirSync: sandbox.spy( () => packageNames ),
+				createReadStream: sandbox.spy( path => `${ path } content` ),
+			},
+		};
+
+		mockery.registerMock( './transifex-service', stubs.transifexService );
+		mockery.registerMock( 'fs', stubs.fs );
+		mockery.registerMock( '@ckeditor/ckeditor5-dev-utils', {
+			logger: () => stubs.logger
+		} );
+
 		sandbox.stub( process, 'cwd', () => path.join( 'workspace', 'ckeditor5' ) );
 
 		upload = require( '../../lib/translations/upload' );
 	} );
 
 	afterEach( () => {
-		mockery.deregisterAll();
+		mockery.disable();
 		sandbox.restore();
 	} );
 
 	it( 'should be able to create and update resources on the Transifex', () => {
-		const packageNames = [
+		packageNames = [
 			'ckeditor5-core',
 			'ckeditor5-ui',
 		];
 
-		const getResourcesSpy = sandbox.spy( () => ( [ {
-			slug: 'ckeditor5-core'
-		} ] ) );
-		const postResourceSpy = sandbox.spy( () => Promise.resolve( '[]' ) );
-		const putResourceContentSpy = sandbox.spy( () => Promise.resolve( '{}' ) );
-
-		sandbox.stub( transifexService, 'getResources', getResourcesSpy );
-		sandbox.stub( transifexService, 'postResource', postResourceSpy );
-		sandbox.stub( transifexService, 'putResourceContent', putResourceContentSpy );
-
-		const readDirSyncStub = sandbox.stub( fs, 'readdirSync', () => packageNames );
-		const createReadStreamStub = sandbox.stub( fs, 'createReadStream', ( path ) => `${path} content` );
-
 		return upload( { username: 'username', password: 'password' } ).then( () => {
-			sinon.assert.calledOnce( getResourcesSpy );
-			sinon.assert.calledTwice( createReadStreamStub );
-			sinon.assert.calledWithExactly( readDirSyncStub, path.join( 'workspace', 'ckeditor5', 'build', '.transifex' ) );
+			sinon.assert.calledOnce( stubs.transifexService.getResources );
+			sinon.assert.calledTwice( stubs.fs.createReadStream );
+			sinon.assert.calledWithExactly( stubs.fs.readdirSync, path.join( 'workspace', 'ckeditor5', 'build', '.transifex' ) );
 
-			sinon.assert.calledOnce( postResourceSpy );
-			sinon.assert.calledWithExactly( postResourceSpy, {
+			sinon.assert.calledOnce( stubs.transifexService.postResource );
+			sinon.assert.calledWithExactly( stubs.transifexService.postResource, {
 				username: 'username',
 				password: 'password',
 				name: 'ckeditor5-ui',
@@ -68,9 +83,9 @@ describe( 'upload', () => {
 				content: 'workspace/ckeditor5/build/.transifex/ckeditor5-ui/en.pot content'
 			} );
 
-			sinon.assert.calledOnce( putResourceContentSpy );
+			sinon.assert.calledOnce( stubs.transifexService.putResourceContent );
 
-			sinon.assert.calledWithExactly( putResourceContentSpy, {
+			sinon.assert.calledWithExactly( stubs.transifexService.putResourceContent, {
 				username: 'username',
 				password: 'password',
 				slug: 'ckeditor5-core',
