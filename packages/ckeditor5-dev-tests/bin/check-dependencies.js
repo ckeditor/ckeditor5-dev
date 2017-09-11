0e201da982730d060463f3fcd715d0904b0756c7
Kamil Piechaczek <pomek@pomek.pl>
2017-09-11 15:32:25 +0200

    Adds "dependency-checker" which allows validating whether required dependencies are specified in package.json.

---
+++ packages/ckeditor5-dev-tests/bin/check-dependencies.js
@@ -0,0 +1,88 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2017, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+const fs = require( 'fs' );
+const path = require( 'path' );
+const glob = require( 'glob' );
+
+const cwd = process.cwd();
+const packageJson = require( path.join( cwd, 'package.json' ) );
+
+const currentPackageName = packageJson.name;
+const dependencies = Object.keys( packageJson.dependencies || {} );
+const devDependencies = Object.keys( packageJson.devDependencies || {} );
+
+const missingDependencies = new Set();
+const missingDevDependencies = new Set();
+const invalidFiles = new Set();
+
+const globPattern = path.join( cwd, '@(src|tests)/**/*.js' );
+
+for ( const filePath of glob.sync( globPattern ) ) {
+	fs.readFileSync( filePath, 'utf-8' )
+		.split( '\n' )
+		.forEach( line => {
+			// Find required package name.
+			let packageName = /^import .* from '(@ckeditor\/[^/]+)\/.*/.exec( line );
+
+			if ( !packageName ) {
+				return;
+			}
+
+			packageName = packageName[ 1 ];
+
+			// Current package cannot be added as dependency.
+			if ( currentPackageName === packageName ) {
+				return;
+			}
+
+			// Check whether the package is defined as dependency and dev-dependency.
+			const containPackageAsDependency = dependencies.includes( packageName );
+			const containPackageAsDevDependency = devDependencies.includes( packageName );
+
+			// If so, current checking line is fine.
+			if ( containPackageAsDependency || containPackageAsDevDependency ) {
+				return;
+			}
+
+			// If found package is missing and current file is "source" file, add the package as a missing dependency.
+			if ( !containPackageAsDependency && filePath.match( /\/src\// ) ) {
+				missingDependencies.add( packageName );
+			}
+
+			// If found package is missing and current file is "test" file, add the package as a missing dev-dependency.
+			if ( !containPackageAsDevDependency && filePath.match( /\/tests\// ) ) {
+				missingDevDependencies.add( packageName );
+			}
+
+			// Mark current file as invalid (it can help for manual debugging).
+			invalidFiles.add( filePath.replace( cwd + '/', './' ) );
+		} );
+}
+
+if ( invalidFiles.size ) {
+	let errorMessage = 'The files listed below contain dependencies which are not defined in "package.json":\n\n';
+	errorMessage += [ ...invalidFiles ].map( formatLine ).join( '\n' );
+
+	if ( missingDependencies.size ) {
+		errorMessage += '\n\nMissing dependencies:\n\n';
+		errorMessage += [ ...missingDependencies ].map( formatLine ).join( '\n' );
+	}
+
+	if ( missingDevDependencies.size ) {
+		errorMessage += '\n\nMissing dev-dependencies:\n\n';
+		errorMessage += [ ...missingDevDependencies ].map( formatLine ).join( '\n' );
+	}
+
+	throw new Error( errorMessage + '\n' );
+}
+
+function formatLine( line ) {
+	return '  - ' + line;
+}
