8feba11aa6908e69141283b2b1fd5e0ea3ff68fe
Kamil Piechaczek <pomek@pomek.pl>
2017-12-18 14:03:03 +0100

    Create a temporary file was moved from "getKarmaConfig" function. Added a basic test. Entry file will not be removed after tests execution.

--- packages/ckeditor5-dev-tests/lib/utils/automated-tests/getkarmaconfig.js
+++ packages/ckeditor5-dev-tests/lib/utils/automated-tests/getkarmaconfig.js
@@ -6,85 +6,32 @@
 /* jshint browser: false, node: true, strict: true */
 'use strict';
 
-const fs = require( 'fs' );
 const path = require( 'path' );
-const glob = require( 'glob' );
-const minimatch = require( 'minimatch' );
-const tmp = require( 'tmp' );
-const karmaLogger = require( 'karma/lib/logger.js' );
 const getWebpackConfigForAutomatedTests = require( './getwebpackconfig' );
-const transformFileOptionToTestGlob = require( '../transformfileoptiontotestglob' );
 
 const AVAILABLE_REPORTERS = [
 	'mocha',
 	'dots'
 ];
 
-// Glob patterns that should be ignored. It means if specified test file
-// match to these patterns, this file will be skipped.
-const IGNORE_GLOBS = [
-	// Ignore files which are saved in `manual/` directory. There are manual tests.
-	path.join( '**', 'tests', '**', 'manual', '**', '*.js' ),
-	// Ignore `_utils` directory as well because there are saved utils for tests.
-	path.join( '**', 'tests', '**', '_utils', '**', '*.js' )
-];
-
 /**
  * @param {Object} options
- * @returns {Object|undefined}
+ * @returns {Object}
  */
 module.exports = function getKarmaConfig( options ) {
-	karmaLogger.setupFromConfig( { logLevel: 'INFO' } );
-
 	const basePath = process.cwd();
 	const coverageDir = path.join( basePath, 'coverage' );
-	const log = karmaLogger.create( 'config' );
-
-	if ( !Array.isArray( options.files ) || options.files.length === 0 ) {
-		return log.error( 'Karma requires files to tests. `options.files` has to be non-empty array.' );
-	}
 
 	if ( !AVAILABLE_REPORTERS.includes( options.reporter ) ) {
-		return log.error( 'Specified reporter is not supported. Available reporters: %s.', AVAILABLE_REPORTERS.join( ', ' ) );
+		throw new Error( `Specified reporter is not supported. Available reporters: ${ AVAILABLE_REPORTERS.join( ', ' ) }.` );
 	}
-
-	const globPatterns = options.files.map( file => transformFileOptionToTestGlob( file ) );
-	const allFiles = [];
-
-	// We want to create a single entry point for Karma.
-	// Every pattern must be resolved before Karma starts work.
-	for ( const singlePattern of globPatterns ) {
-		const files = glob.sync( singlePattern );
-
-		if ( !files.length ) {
-			log.warn( 'Pattern "%s" does not match any file.', singlePattern );
-		}
-
-		allFiles.push(
-			...files.filter( file => !IGNORE_GLOBS.some( globPattern => minimatch( file, globPattern ) ) )
-		);
-	}
-
-	if ( !allFiles.length ) {
-		return log.error( 'Not found files to tests. Specified patterns are invalid.' );
-	}
-
-	const tempFile = tmp.fileSync( { postfix: '.js' } );
-
-	const filesImports = allFiles
-		.map( file => 'import "' + file + '";' )
-		.join( '\n' );
-
-	fs.writeFileSync( tempFile.name, filesImports );
-
-	log.info( 'Entry file saved in "%s".', tempFile.name );
 
 	const preprocessorMap = {
-		[ tempFile.name ]: [ 'webpack' ]
+		[ options.entryFile ]: [ 'webpack' ]
 	};
 
 	if ( options.sourceMap ) {
-		preprocessorMap[ tempFile.name ].push( 'sourcemap' );
+		preprocessorMap[ options.entryFile ].push( 'sourcemap' );
 	}
 
 	const karmaConfig = {
@@ -95,14 +42,14 @@ module.exports = function getKarmaConfig( options ) {
 		frameworks: [ 'mocha', 'chai', 'sinon' ],
 
 		// List of files/patterns to load in the browser.
-		files: [ tempFile.name ],
+		files: [ options.entryFile ],
 
 		// Preprocess matching files before serving them to the browser.
 		// Available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: preprocessorMap,
 
 		webpack: getWebpackConfigForAutomatedTests( {
-			files: globPatterns,
+			files: Object.keys( options.globPatterns ).map( key => options.globPatterns[ key ] ),
 			sourceMap: options.sourceMap,
 			coverage: options.coverage,
 			themePath: options.themePath
@@ -156,10 +103,6 @@ module.exports = function getKarmaConfig( options ) {
 		// Shows differences in object comparison.
 		mochaReporter: {
 			showDiff: true
-		},
-
-		removeEntryFile() {
-			return tempFile.removeCallback();
 		}
 	};
 
--- packages/ckeditor5-dev-tests/tests/utils/automated-tests/getkarmaconfig.js
+++ packages/ckeditor5-dev-tests/tests/utils/automated-tests/getkarmaconfig.js
@@ -9,121 +9,39 @@ const mockery = require( 'mockery' );
 const { expect } = require( 'chai' );
 const sinon = require( 'sinon' );
 const path = require( 'path' );
-const proxyquire = require( 'proxyquire' );
 
 describe( 'getKarmaConfig', () => {
-	let getKarmaConfig, sandbox, stubs;
+	let getKarmaConfig, sandbox;
 	const originalEnv = process.env;
 
 	beforeEach( () => {
 		sandbox = sinon.sandbox.create();
+
 		sandbox.stub( process, 'cwd' ).returns( 'workspace' );
 		sandbox.stub( path, 'join' ).callsFake( ( ...chunks ) => chunks.join( '/' ) );
-		sandbox.stub( path, 'sep' ).value( '/' );
+
 		// Sinon cannot stub non-existing props.
 		process.env = Object.assign( {}, originalEnv, { TRAVIS: false } );
-
-		stubs = {
-			fs: {
-				writeFileSync: sandbox.stub()
-			},
-			log: {
-				info: sandbox.stub(),
-				warn: sandbox.stub(),
-				error: sandbox.stub(),
-			},
-			glob: {
-				sync: sandbox.stub()
-			},
-			tmp: {
-				fileSync: sandbox.stub().callsFake( () => stubs.tmp._fileSync ),
-				_fileSync: {
-					name: '',
-					removeCallback: sandbox.stub()
-				}
-			}
-		};
 
 		mockery.enable( {
 			warnOnReplace: false,
 			warnOnUnregistered: false
 		} );
 
-		mockery.registerMock( 'karma/lib/logger.js', {
-			setupFromConfig: sandbox.spy(),
-			create( name ) {
-				expect( name ).to.equal( 'config' );
-
-				return stubs.log;
-			}
-		} );
 		mockery.registerMock( './getwebpackconfig', options => options );
 
-		getKarmaConfig = proxyquire( '../../../lib/utils/automated-tests/getkarmaconfig', {
-			tmp: stubs.tmp,
-			fs: stubs.fs,
-			glob: stubs.glob
-		} );
+		getKarmaConfig = require( '../../../lib/utils/automated-tests/getkarmaconfig' );
 	} );
 
 	afterEach( () => {
 		sandbox.restore();
 		mockery.disable();
 		mockery.deregisterAll();
+
 		process.env = originalEnv;
 	} );
 
-	it( 'should warn if specified pattern is invalid', () => {
-		stubs.glob.sync.returns( [] );
-
-		getKarmaConfig( {
-			files: [ 'invalid-package' ],
-			reporter: 'mocha',
-			sourceMap: false,
-			coverage: false,
-			browsers: [ 'Chrome' ],
-			watch: false,
-			verbose: false
-		} );
-
-		expect( stubs.log.warn.calledOnce ).to.equal( true );
-		expect( stubs.log.warn.firstCall.args[ 0 ] ).to.equal( 'Pattern "%s" does not match any file.' );
-		expect( stubs.log.warn.firstCall.args[ 1 ] ).to.equal( 'workspace/packages/ckeditor5-invalid-package/tests/**/*.js' );
-	} );
-
-	it( 'should log error if not found files to tests', () => {
-		stubs.glob.sync.returns( [] );
-
-		getKarmaConfig( {
-			files: [ 'invalid-package', 'invalid/foo.js' ],
-			reporter: 'mocha',
-			sourceMap: false,
-			coverage: false,
-			browsers: [ 'Chrome' ],
-			watch: false,
-			verbose: false
-		} );
-
-		expect( stubs.log.warn.calledTwice ).to.equal( true );
-		expect( stubs.log.warn.firstCall.args[ 0 ] ).to.equal( 'Pattern "%s" does not match any file.' );
-		expect( stubs.log.warn.firstCall.args[ 1 ] ).to.equal( 'workspace/packages/ckeditor5-invalid-package/tests/**/*.js' );
-		expect( stubs.log.warn.secondCall.args[ 0 ] ).to.equal( 'Pattern "%s" does not match any file.' );
-		expect( stubs.log.warn.secondCall.args[ 1 ] ).to.equal( 'workspace/packages/ckeditor5-invalid/tests/foo.js' );
-		expect( stubs.log.error.calledOnce ).to.equal( true );
-		expect( stubs.log.error.firstCall.args[ 0 ] ).to.equal( 'Not found files to tests. Specified patterns are invalid.' );
-	} );
-
 	it( 'should return basic karma config for all tested files', () => {
-		const allFiles = [
-			'workspace/packages/ckeditor5-autoformat/tests/foo.js',
-			'workspace/packages/ckeditor5-basic-styles/tests/bar.js',
-			'workspace/packages/ckeditor5-engine/tests/foo/bar.js'
-		];
-
-		stubs.glob.sync.returns( allFiles );
-
-		sandbox.stub( stubs.tmp._fileSync, 'name' ).value( '/tmp/file' );
-
 		const karmaConfig = getKarmaConfig( {
 			files: [ '*' ],
 			reporter: 'mocha',
@@ -131,20 +49,13 @@ describe( 'getKarmaConfig', () => {
 			coverage: false,
 			browsers: [ 'Chrome' ],
 			watch: false,
-			verbose: false
+			verbose: false,
+			themePath: 'workspace/path/to/theme.css',
+			entryFile: 'workspace/entry-file.js',
+			globPatterns: {
+				'*': 'workspace/packages/ckeditor5-*/tests/**/*.js'
+			}
 		} );
-
-		expect( stubs.fs.writeFileSync.calledOnce ).to.equal( true );
-		expect( stubs.fs.writeFileSync.firstCall.args[ 0 ] ).to.equal( '/tmp/file' );
-		expect( stubs.fs.writeFileSync.firstCall.args[ 1 ] ).to.equal( [
-			'import "workspace/packages/ckeditor5-autoformat/tests/foo.js";',
-			'import "workspace/packages/ckeditor5-basic-styles/tests/bar.js";',
-			'import "workspace/packages/ckeditor5-engine/tests/foo/bar.js";'
-		].join( '\n' ) );
-
-		expect( stubs.log.info.calledOnce ).to.equal( true );
-		expect( stubs.log.info.firstCall.args[ 0 ] ).to.equal( 'Entry file saved in "%s".' );
-		expect( stubs.log.info.firstCall.args[ 1 ] ).to.equal( '/tmp/file' );
 
 		expect( karmaConfig ).to.have.own.property( 'basePath', 'workspace' );
 		expect( karmaConfig ).to.have.own.property( 'frameworks' );
@@ -154,45 +65,10 @@ describe( 'getKarmaConfig', () => {
 		expect( karmaConfig.webpack.files ).to.deep.equal( [ 'workspace/packages/ckeditor5-*/tests/**/*.js' ] );
 		expect( karmaConfig.webpack.sourceMap ).to.equal( false );
 		expect( karmaConfig.webpack.coverage ).to.equal( false );
+		expect( karmaConfig.webpack.themePath ).to.equal( 'workspace/path/to/theme.css' );
 		expect( karmaConfig ).to.have.own.property( 'webpackMiddleware' );
 		expect( karmaConfig ).to.have.own.property( 'reporters' );
 		expect( karmaConfig ).to.have.own.property( 'browsers' );
 		expect( karmaConfig ).to.have.own.property( 'singleRun', true );
-	} );
-
-	it( 'allows removing temporary entry point', () => {
-		const allFiles = [
-			'workspace/packages/ckeditor5-autoformat/tests/foo.js',
-			'workspace/packages/ckeditor5-basic-styles/tests/bar.js',
-			'workspace/packages/ckeditor5-engine/tests/foo/bar.js'
-		];
-
-		stubs.glob.sync.returns( allFiles );
-
-		sandbox.stub( stubs.tmp._fileSync, 'name' ).value( '/tmp/file' );
-
-		const karmaConfig = getKarmaConfig( {
-			files: [ '*' ],
-			reporter: 'mocha',
-			sourceMap: false,
-			coverage: false,
-			browsers: [ 'Chrome' ],
-			watch: false,
-			verbose: false
-		} );
-
-		expect( karmaConfig.removeEntryFile ).to.be.a( 'function' );
-		expect( stubs.tmp._fileSync.removeCallback.called ).to.equal( false );
-		karmaConfig.removeEntryFile();
-		expect( stubs.tmp._fileSync.removeCallback.called ).to.equal( true );
-	} );
-
-	it( 'should log error if no files are provided', () => {
-		getKarmaConfig( { reporter: 'mocha' } );
-
-		expect( stubs.log.error.calledOnce ).to.equal( true );
-		expect( stubs.log.error.firstCall.args[ 0 ] ).to.equal(
-			'Karma requires files to tests. `options.files` has to be non-empty array.'
-		);
 	} );
 } );
