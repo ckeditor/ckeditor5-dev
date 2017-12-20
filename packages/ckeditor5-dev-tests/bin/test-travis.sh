fd86288d0cb2a657b327129d3a61d46490d9de8f
Kamil Piechaczek <pomek@pomek.pl>
2017-12-20 09:08:25 +0100

    Turn on the BrowserStack on Travis.

--- packages/ckeditor5-dev-tests/bin/test-travis.sh
+++ packages/ckeditor5-dev-tests/bin/test-travis.sh
@@ -2,4 +2,5 @@
 
 npm run lint && \
 node_modules/.bin/ckeditor5-dev-tests-check-dependencies && \
-node --max_old_space_size=4096 node_modules/.bin/ckeditor5-dev-tests --coverage --reporter=dots
+node --max_old_space_size=4096 node_modules/.bin/ckeditor5-dev-tests --coverage --reporter=dots --browsers=Chrome && \
+node --max_old_space_size=4096 node_modules/.bin/ckeditor5-dev-tests --reporter=dots --browsers=Firefox,BrowserStack_Edge,BrowserStack_Safari
