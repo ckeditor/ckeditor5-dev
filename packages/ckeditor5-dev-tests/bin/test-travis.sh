701bd1177da8c68875426b1054e41eed6d4fd457
Kamil Piechaczek <pomek@pomek.pl>
2017-12-20 09:24:19 +0100

    Enabled linter.

--- packages/ckeditor5-dev-tests/bin/test-travis.sh
+++ packages/ckeditor5-dev-tests/bin/test-travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#npm run lint && \
-#node_modules/.bin/ckeditor5-dev-tests-check-dependencies && \
+npm run lint && \
+node_modules/.bin/ckeditor5-dev-tests-check-dependencies && \
 node --max_old_space_size=4096 node_modules/.bin/ckeditor5-dev-tests --coverage --reporter=dots --browsers=Chrome && \
 node --max_old_space_size=4096 node_modules/.bin/ckeditor5-dev-tests --reporter=dots --browsers=Firefox,BrowserStack_Edge,BrowserStack_Safari
