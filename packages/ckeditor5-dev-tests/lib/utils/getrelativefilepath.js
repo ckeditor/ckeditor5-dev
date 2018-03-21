b4da9d0c2695c6c27cd29eb709624d2da851569a
Kamil Piechaczek <pomek@pomek.pl>
2018-03-21 11:15:41 +0100

    Added tests for "getRelativeFilePath()" function.

--- packages/ckeditor5-dev-tests/lib/utils/getrelativefilepath.js
+++ packages/ckeditor5-dev-tests/lib/utils/getrelativefilepath.js
@@ -3,15 +3,18 @@
  * For licensing, see LICENSE.md.
  */
 
-/* jshint node: true, strict: true */
-
 'use strict';
 
-// Get a path to a source file which will uniquely identify this file in
-// a build directory, once all package are gathered together.
-//
-// In order to do that, everything up to `ckeditor-*/` is removed:
-// /work/space/ckeditor5-foo/tests/manual/foo.js -> ckeditor5-foo/tests/manual/foo.js
+/**
+ * Get a path to a source file which will uniquely identify this file in
+ * a build directory, once all packages are gathered together.
+ *
+ * In order to do that, everything up to `ckeditor5-packageName` is removed:
+ * /work/space/ckeditor5-foo/tests/manual/foo.js -> ckeditor5-foo/tests/manual/foo.js
+ *
+ * @param {String} filePath
+ * @returns {String}
+ */
 module.exports = function getRelativeFilePath( filePath ) {
 	return filePath.replace( /^.+[/\\]ckeditor5-/, 'ckeditor5-' );
 };
