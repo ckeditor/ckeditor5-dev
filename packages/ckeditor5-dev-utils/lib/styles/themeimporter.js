6159b6b108c1ad09640d84235951fb23357a2d7a
Kamil Piechaczek <pomek@pomek.pl>
2018-04-24 09:43:53 +0200

    Added a comment with some explanation.

--- packages/ckeditor5-dev-utils/lib/styles/themeimporter.js
+++ packages/ckeditor5-dev-utils/lib/styles/themeimporter.js
@@ -119,6 +119,8 @@ function importFile( options ) {
 				type: 'dependency'
 			} );
 
+			// `importResult` contains references to all dependencies that were used.
+			// We need to inform the base file (the file which imports the *.css file) that these dependencies should be watched too.
 			importResult.messages.forEach( message => {
 				result.messages.push( message );
 			} );
