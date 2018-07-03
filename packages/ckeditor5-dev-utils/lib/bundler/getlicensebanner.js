0c7e85618763a5f94b97b432a07f9a01ea622664
Maciej Bukowski <ma2ciek@gmail.com>
2018-07-03 14:46:30 +0200

    Added comment for the special banner.

--- packages/ckeditor5-dev-utils/lib/bundler/getlicensebanner.js
+++ packages/ckeditor5-dev-utils/lib/bundler/getlicensebanner.js
@@ -8,6 +8,10 @@
 module.exports = function getLicenseBanner() {
 	const date = new Date();
 
+	// License banner starts with `!`. That combines with uglifyjs' `comments` /^!/ option
+	// make webpack preserve that banner while cleaning code from others comments during the build task.
+	// It's because UglifyJsWebpackPlugin minification takes place after adding a banner.
+
 	/* eslint-disable indent */
 	return (
 `/*!
