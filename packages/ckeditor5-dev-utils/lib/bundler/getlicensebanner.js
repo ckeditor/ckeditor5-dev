1bbdec99aa060a6567f815895964ee1701ad6861
Maciej Bukowski <ma2ciek@gmail.com>
2018-07-03 13:32:44 +0200

    Switched back to banner with exclamation mark.

--- packages/ckeditor5-dev-utils/lib/bundler/getlicensebanner.js
+++ packages/ckeditor5-dev-utils/lib/bundler/getlicensebanner.js
@@ -10,10 +10,10 @@ module.exports = function getLicenseBanner() {
 
 	/* eslint-disable indent */
 	return (
-`/*--------------------------------------------------------------------------------------*
- * @license Copyright (c) 2003-${ date.getFullYear() }, CKSource - Frederico Knabben. All rights reserved. *
- * For licensing, see LICENSE.md.                                                       *
- *--------------------------------------------------------------------------------------*/`
+`/*!
+ * @license Copyright (c) 2003-${ date.getFullYear() }, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */`
 	);
 	/* eslint-enable indent */
 };
