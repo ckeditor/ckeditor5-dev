10a7934cbb0e5bdf7d35c4bdc09ffa64e1f985da
Kamil Piechaczek <pomek@pomek.pl>
2018-10-30 14:47:14 +0100

    Aligned to changes in mgit.

--- packages/ckeditor5-dev-tests/bin/install-dependencies.sh
+++ packages/ckeditor5-dev-tests/bin/install-dependencies.sh
@@ -7,7 +7,7 @@ npm install mgit2 lerna@^2.0.0-rc.1 eslint-config-ckeditor5 karma-coveralls@^1.1
 node_modules/.bin/ckeditor5-dev-tests-create-mgit-json
 node_modules/.bin/ckeditor5-dev-tests-create-lerna-json
 
-mgit bootstrap --recursive --resolver-path=@ckeditor/ckeditor5-dev-tests/lib/mgit-resolver.js
+mgit sync --recursive --resolver-path=@ckeditor/ckeditor5-dev-tests/lib/mgit-resolver.js
 
 # We need to ignore the newly created packages dir with all its content (see #203).
 echo -e "\npackages/**\n" >> .gitignore
