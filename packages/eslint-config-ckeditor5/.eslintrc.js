b227c2da8a7b7de06ce04ae155db58858d838bd7
Kamil Piechaczek <pomek@pomek.pl>
2018-11-28 13:34:14 +0100

    Enabled the new plugin in our eslint config.

--- packages/eslint-config-ckeditor5/.eslintrc.js
+++ packages/eslint-config-ckeditor5/.eslintrc.js
@@ -19,9 +19,9 @@ module.exports = {
 		expect: true,
 		sinon: true
 	},
-	// plugins: [
-	// 	'ckeditor5-rules'
-	// ],
+	plugins: [
+		'ckeditor5-rules'
+	],
 	rules: {
 		// ## Possible errors
 		// Offing because we keep the browser env disabled so when using a console you need to define
@@ -291,7 +291,7 @@ module.exports = {
 		"yield-star-spacing": [
 			"error",
 			"after"
-		]
-		// 'ckeditor5-rules/no-relative-imports': 'error'
+		],
+		'ckeditor5-rules/no-relative-imports': 'error'
 	}
 };
