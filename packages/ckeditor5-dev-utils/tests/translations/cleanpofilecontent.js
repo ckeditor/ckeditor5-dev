28e3b533d9667cbfda45f09a37e103f171649ff0
Kamil Piechaczek <pomek@pomek.pl>
2019-01-24 12:03:31 +0100

    Internal: Bumped the year.

--- packages/ckeditor5-dev-utils/tests/translations/cleanpofilecontent.js
+++ packages/ckeditor5-dev-utils/tests/translations/cleanpofilecontent.js
@@ -1,5 +1,5 @@
 /**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md.
  */
 
@@ -14,7 +14,7 @@ describe( 'translations', () => {
 		it( 'clean po files from personal data and add the special header', () => {
 			const poFileContent =
 // eslint-disable-next-line max-len
-`# Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+`# Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
 # Translators:
 # Xuxxx Satxxx <xxxx@gmail.com>, 2017
 msgid ""
@@ -31,7 +31,7 @@ msgstr "URL del enllaz"
 
 			const expectedResult =
 // eslint-disable-next-line max-len
-`# Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
+`# Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
 #
 #                                     !!! IMPORTANT !!!
 #
