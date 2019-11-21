6199b8937d89df3f61298f49834806da332408ac
Maciej Bukowski <ma2ciek@gmail.com>
2019-11-21 13:08:55 +0100

    Fixed typo.

--- packages/ckeditor5-dev-tests/lib/utils/ck-debug-loader.js
+++ packages/ckeditor5-dev-tests/lib/utils/ck-debug-loader.js
@@ -11,7 +11,7 @@ module.exports = function ckDebugLoader( source, map ) {
 	source = source.replace( /\/\/ @if (!?[\w]+) \/\/(.+)/g, ( match, flagName, body ) => {
 		// `this.query` comes from the webpack loader configuration.
 
-		// Do not unccoment the code if the flag is missing / falsy.
+		// Do not uncomment the code if the flag is missing / falsy.
 		if ( !this.query.debugFlags.includes( flagName ) ) {
 			return match;
 		}
