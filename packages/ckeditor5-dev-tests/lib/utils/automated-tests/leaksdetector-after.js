2943b9dc2d9b1feee8c4e893e67a2c4c4fdda480
fredck <f.knabben@cksource.com>
2020-02-23 02:06:29 +0100

    Broke the leak detector hooks into before/after so they can be loaded properly.

---
+++ packages/ckeditor5-dev-tests/lib/utils/automated-tests/leaksdetector-after.js
@@ -0,0 +1,22 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* global window, document */
+
+afterEach( function() {
+	if ( document.body.childElementCount !== this._lastDomElementsCount ) {
+		const leaksCount = document.body.childElementCount - this._lastDomElementsCount;
+		const errorMessage = `${ leaksCount } elements were leaked. Be a good citizen and clean your DOM once you're done with it.`;
+
+		this._lastDomElementsCount = document.body.childElementCount;
+
+		// See https://github.com/ckeditor/ckeditor5-dev/issues/586#issuecomment-571573488.
+		if ( window.production ) {
+			throw new Error( errorMessage );
+		} else {
+			console.error( errorMessage );
+		}
+	}
+} );
