47f235c5b48c408792ebfbe99d06555386d283f0
Maciej Bukowski <ma2ciek@gmail.com>
2020-07-14 09:06:52 +0200

    Added a plugin that fixes code snippet padding in doclet descriptions.

---
+++ packages/jsdoc-plugins/lib/fix-code-snippets.js
@@ -0,0 +1,47 @@
+/**
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * Licensed under the terms of the MIT License (see LICENSE.md).
+ */
+
+/**
+ * A fixer for doclet descriptions including code snippets.
+ * After upgrading JSDoc to v. 3.6.4 snippets started to contain leading tabs or spaces.
+ * This plugin removes that left padding.
+ */
+exports.handlers = {
+	parseComplete: e => {
+		for ( const doclet of e.doclets ) {
+			if ( !doclet.description ) {
+				continue;
+			}
+
+			doclet.description = doclet.description.replace( /<pre><code>(.*?)<\/code><\/pre>/gs, ( _match, content ) => {
+				const codeRows = content.split( '\n' );
+
+				let paddingSize = 0;
+
+				while ( true ) {
+					const paddingText = codeRows[ 0 ].slice( 0, paddingSize + 1 );
+
+					// When some row starts with a different text or
+					// padding contains some non-whitespace characters
+					// then it means that it is no loner a padding.
+					if (
+						( codeRows.some( row => /\S/.test( row ) && !row.startsWith( paddingText ) ) ) ||
+						/\S/.test( paddingText )
+					) {
+						break;
+					}
+
+					paddingSize++;
+				}
+
+				return (
+					'<pre><code>' +
+					codeRows.map( row => row.slice( paddingSize ) ).join( '\n' ) +
+					'</code></pre>'
+				);
+			} );
+		}
+	}
+};
