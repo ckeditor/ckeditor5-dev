c35a754d02695bb7a14a67aeeeaec36b0e545f2c
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-05-20 15:19:05 +0200

    Removed wrong first line in web-crawler files.

--- packages/ckeditor5-dev-docs/lib/web-crawler/constants.js
+++ packages/ckeditor5-dev-docs/lib/web-crawler/constants.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /**
  * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
