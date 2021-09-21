7bfb2f8ecadcaf18c8cb738298a2fe8d1f429d7d
Kamil Piechaczek <pomek@users.noreply.github.com>
2021-09-21 11:22:29 +0200

    Hide logs when running the HTTP server.

--- packages/ckeditor5-dev-tests/tests/utils/manual-tests/createserver.js
+++ packages/ckeditor5-dev-tests/tests/utils/manual-tests/createserver.js
@@ -9,20 +9,37 @@ const http = require( 'http' );
 const { use, expect } = require( 'chai' );
 const sinon = require( 'sinon' );
 const sinonChai = require( 'sinon-chai' );
+const mockery = require( 'mockery' );
 
 use( sinonChai );
 
 describe( 'createManualTestServer', () => {
-	let sandbox, httpCreateServerStub, createManualTestServer, server;
+	let sandbox, httpCreateServerStub, createManualTestServer, server, loggerStub;
 
 	beforeEach( () => {
 		sandbox = sinon.createSandbox();
+
+		mockery.enable( {
+			useCleanCache: true,
+			warnOnReplace: false,
+			warnOnUnregistered: false
+		} );
+
+		loggerStub = sinon.stub();
 
 		httpCreateServerStub = sandbox.stub( http, 'createServer' ).callsFake( function stubbedCreateServer( ...theArgs ) {
 			server = httpCreateServerStub.wrappedMethod( ...theArgs );
 			sandbox.spy( server, 'listen' );
 
 			return server;
+		} );
+
+		mockery.registerMock( '@ckeditor/ckeditor5-dev-utils', {
+			logger() {
+				return {
+					info: loggerStub
+				};
+			}
 		} );
 
 		createManualTestServer = require( '../../../lib/utils/manual-tests/createserver' );
@@ -38,6 +55,8 @@ describe( 'createManualTestServer', () => {
 		if ( server._readline ) {
 			server._readline.close();
 		}
+
+		mockery.disable();
 	} );
 
 	it( 'should start http server', () => {
@@ -50,11 +69,17 @@ describe( 'createManualTestServer', () => {
 		createManualTestServer( 'workspace/build/.manual-tests', 8888 );
 
 		expect( httpCreateServerStub.returnValues[ 0 ].listen ).to.be.calledOnceWith( 8888 );
+
+		expect( loggerStub.calledOnce ).to.equal( true );
+		expect( loggerStub.firstCall.firstArg ).to.equal( '[Server] Server running at http://localhost:8888/' );
 	} );
 
 	it( 'should listen on 8125 port if no specific port was given', () => {
 		createManualTestServer( 'workspace/build/.manual-tests' );
 
 		expect( httpCreateServerStub.returnValues[ 0 ].listen ).to.be.calledOnceWith( 8125 );
+
+		expect( loggerStub.calledOnce ).to.equal( true );
+		expect( loggerStub.firstCall.firstArg ).to.equal( '[Server] Server running at http://localhost:8125/' );
 	} );
 } );
