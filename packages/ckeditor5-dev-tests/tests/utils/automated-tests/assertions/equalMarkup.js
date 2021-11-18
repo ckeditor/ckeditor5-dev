2c210b0f2837cfe5ae6dc4d29851ccfd57f9bbbc
Przemyslaw Zan <p.zan@cksource.com>
2021-11-18 18:32:06 +0100

    WIP - Commented tests for now

--- packages/ckeditor5-dev-tests/tests/utils/automated-tests/assertions/equalMarkup.js
+++ packages/ckeditor5-dev-tests/tests/utils/automated-tests/assertions/equalMarkup.js
@@ -5,7 +5,7 @@
 
 const chai = require( 'chai' );
 
-require( '../../../../lib/utils/automated-tests/assertions/equalMarkup' );
+// require( '../../../../lib/utils/automated-tests/assertions/equalMarkup' );
 
 const markupA = '<paragraph>foo bXXX[]r baz</paragraph>';
 const markupB = '<paragraph>foo bYYY[]r baz</paragraph>';
@@ -30,3 +30,107 @@ describe( 'equalMarkup chai assertion', () => {
 		} ).to.throw( 'Expected markup strings to be equal' );
 	} );
 } );
+
+/*
+import AssertionError from 'assertion-error';
+
+describe( 'assertEqualMarkup()', () => {
+	it( 'should not throw for equal strings', () => {
+		expect( assertEqualMarkup( 'foo', 'foo' ) ).to.not.throw;
+	} );
+
+	it( 'should throw AssertionError for not equal strings', () => {
+		try {
+			assertEqualMarkup( 'foo', 'bar' );
+		} catch ( assertionError ) {
+			expect( assertionError ).to.be.instanceOf( AssertionError );
+		}
+	} );
+
+	it( 'should throw with default (short) message', () => {
+		try {
+			assertEqualMarkup( 'foo', 'bar' );
+		} catch ( assertionError ) {
+			expect( assertionError.message ).to.equal( 'Expected markup strings to be equal' );
+		}
+	} );
+
+	it( 'should throw with passed message', () => {
+		try {
+			assertEqualMarkup( 'foo', 'bar', 'baz' );
+		} catch ( assertionError ) {
+			expect( assertionError.message ).to.equal( 'baz' );
+		}
+	} );
+
+	it( 'should format actual string', () => {
+		try {
+			assertEqualMarkup( '<div><p><span>foo</span></p></div>', 'bar' );
+		} catch ( assertionError ) {
+			expect( assertionError.actual ).to.equal(
+				'<div>\n' +
+				'  <p><span>foo</span></p>\n' +
+				'</div>'
+			);
+		}
+	} );
+
+	it( 'should format expected string', () => {
+		try {
+			assertEqualMarkup( 'foo', '<div><p><span>foo</span></p></div>' );
+		} catch ( assertionError ) {
+			expect( assertionError.expected ).to.equal(
+				'<div>\n' +
+				'  <p><span>foo</span></p>\n' +
+				'</div>'
+			);
+		}
+	} );
+
+	it( 'should format model text node with attributes as inline', () => {
+		try {
+			assertEqualMarkup( 'foo', '<paragraph><$text bold="true">foo</$text></paragraph>' );
+		} catch ( assertionError ) {
+			expect( assertionError.expected ).to.equal(
+				'<paragraph><$text bold="true">foo</$text></paragraph>'
+			);
+		}
+	} );
+
+	it( 'should format nested model structure properly', () => {
+		try {
+			assertEqualMarkup( 'foo',
+				'<blockQuote>' +
+					'<table>' +
+						'<tableRow>' +
+							'<tableCell>' +
+								'<paragraph><$text bold="true">foo</$text></paragraph>' +
+							'</tableCell>' +
+							'<tableCell>' +
+								'<paragraph><$text bold="true">bar</$text></paragraph>' +
+								'<paragraph><$text bold="true">baz</$text></paragraph>' +
+							'</tableCell>' +
+						'</tableRow>' +
+					'</table>' +
+				'</blockQuote>'
+			);
+		} catch ( assertionError ) {
+			expect( assertionError.expected ).to.equal(
+				'<blockQuote>\n' +
+				'  <table>\n' +
+				'    <tableRow>\n' +
+				'      <tableCell>\n' +
+				'        <paragraph><$text bold="true">foo</$text></paragraph>\n' +
+				'      </tableCell>\n' +
+				'      <tableCell>\n' +
+				'        <paragraph><$text bold="true">bar</$text></paragraph>\n' +
+				'        <paragraph><$text bold="true">baz</$text></paragraph>\n' +
+				'      </tableCell>\n' +
+				'    </tableRow>\n' +
+				'  </table>\n' +
+				'</blockQuote>'
+			);
+		}
+	} );
+} );
+*/
