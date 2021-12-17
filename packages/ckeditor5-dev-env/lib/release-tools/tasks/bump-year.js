db53806ab9acbb090b4197dc3804e9e6ea4d6640
Przemyslaw Zan <p.zan@cksource.com>
2021-12-17 17:32:03 +0100

    Refractoring based on feedback

--- packages/ckeditor5-dev-env/lib/release-tools/tasks/bump-year.js
+++ packages/ckeditor5-dev-env/lib/release-tools/tasks/bump-year.js
@@ -5,69 +5,107 @@
 
 'use strict';
 
+const chalk = require( 'chalk' );
 const fs = require( 'fs' );
 const glob = require( 'glob' );
 const minimatch = require( 'minimatch' );
 
 /**
- * See https://github.com/ckeditor/ckeditor5/issues/10909
+ * Updates year in all licenses in the provided directory, based on provided glob patterns.
+ *
+ * Replaces:
+ * 	Copyright (c) [initial year]-xxxx
+ *
+ * With:
+ *	Copyright (c) [initial year]-[current year]
  *
  * @param {Object} params
  * @param {String} params.cwd Current working directory from which all paths will be resolved
- * @param {Function} params.callback Optional function to modify the license header
  * @param {Number} params.initialYear Year from which the licenses should begin
  * @param {Array} params.globPatterns array of objects, where each object has string property 'pattern',
  * and optionally 'options' property.
  */
 module.exports = function bumpYear( params ) {
-	params.globPatterns
+	const filesToUpdate = params.globPatterns
 		.map( globPattern => glob.sync( globPattern.pattern, globPattern.options ) )
 		.reduce( ( previous, current ) => [ ...previous, ...current ] )
 		.filter( fileName => {
-			// Filter out nested `node_modules`.
-			if ( minimatch( fileName, '**/node_modules/**' ) ) {
-				return false;
-			}
-
-			// Filter out stuff from `src/lib/`.
-			if ( minimatch( fileName, '**/src/lib/**' ) ) {
-				return false;
-			}
-
-			// Filter out builds.
-			if ( minimatch( fileName, '**/ckeditor5-build-*/**' ) ||
-				minimatch( fileName, '**/build/**' ) ) {
-				return false;
-			}
-
 			// Filter out directories.
 			if ( fs.statSync( fileName ).isDirectory() ) {
 				return false;
 			}
 
-			return true;
-		} )
-		.forEach( fileName => {
-			const currentYear = new Date().getFullYear();
-			const yearOnlyRegexp = /(?<=^\/\*\*$[\s\S]+^ \* @license.+)(\d{4}-\d{4})(?=[\s\S]+^ \*\/$)/m;
-			const fullLicenseRegexp = /^\/\*\*$[\s\S]+^ \* @license.+\d{4}-\d{4}[\s\S]+^ \*\/$/m;
-
-			const fileContent = fs.readFileSync( fileName, 'utf-8' );
-
-			let updatedFileContent = fileContent.replace( yearOnlyRegexp, `${ params.initialYear }-${ currentYear }` );
-
-			if ( params.callback ) {
-				updatedFileContent = updatedFileContent.replace( fullLicenseRegexp, params.callback );
+			// Filter out nested `node_modules`.
+			if ( minimatch( fileName, '**/node_modules/**' ) ) {
+				return false;
 			}
 
-			if ( fileContent === updatedFileContent ) {
+			// Filter out builds.
+			if ( minimatch( fileName, '**/build/**' ) ) {
+				return false;
+			}
+
+			return true;
+		} );
+
+	const filesWithoutHeader = [];
+	const totalFiles = filesToUpdate.length;
+	let updatedFiles = 0;
+
+	filesToUpdate.forEach( fileName => {
+		const fileContent = fs.readFileSync( fileName, 'utf-8' );
+		const currentYear = new Date().getFullYear();
+
+		// This syntax has to be used in order to insert variable into the regex.
+		const yearRegexp = new RegExp( `(?<=Copyright \\(c\\) ${ params.initialYear }-)\\d{4}`, 'gm' );
+
+		let updatedFileContent = fileContent.replace( yearRegexp, currentYear );
+
+		// TODO: Remove this line after all repositories are updated.
+		updatedFileContent = updatedFileContent.replace( /- Frederico Knabben/, 'Holding sp. z o.o' );
+
+		// Optional replacing of second line with either longer or shorter version.
+		/* Short to long
+		updatedFileContent = updatedFileContent.replace(
+			/For licensing, see LICENSE\.md\./,
+			'For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license'
+		); */
+		/* Long to short
+		updatedFileContent = updatedFileContent.replace(
+			/For licensing, see LICENSE\.md or https:\/\/ckeditor\.com\/legal\/ckeditor-oss-license/,
+			'For licensing, see LICENSE.md.'
+		); */
+
+		if ( fileContent === updatedFileContent ) {
 			// License headers are only required in JS files.
 			// Also, the file might have already been updated.
-				if ( fileName.endsWith( '.js' ) && !fileContent.match( yearOnlyRegexp ) ) {
-					console.warn( `The file "${ params.cwd }/${ fileName }" misses a license header.` );
-				}
-			} else {
-				fs.writeFileSync( fileName, updatedFileContent, 'utf-8' );
+			if ( fileName.endsWith( '.js' ) && !fileContent.match( yearRegexp ) ) {
+				filesWithoutHeader.push( `${ params.cwd }/${ fileName }` );
 			}
-		} );
+		} else {
+			fs.writeFileSync( fileName, updatedFileContent, 'utf-8' );
+		}
+
+		updatedFiles++;
+
+		const percent = ( ( updatedFiles / totalFiles ) * 100 ).toFixed( 2 );
+		const trimmedFileName = fileName.length < 50 ? fileName : '...' + fileName.slice( -47 );
+
+		const output = [
+			`[ ${ updatedFiles } / ${ totalFiles } ]`,
+			chalk.green( `(${ percent }%)` ),
+			chalk.underline( trimmedFileName )
+		].join( ' ' );
+
+		process.stdout.clearLine();
+		process.stdout.cursorTo( 0 );
+		process.stdout.write( output );
+	} );
+
+	if ( filesWithoutHeader.length ) {
+		console.warn( chalk.red( 'Following files are missing their license headers:' ) );
+		for ( const file of filesWithoutHeader ) {
+			console.log( file );
+		}
+	}
 };
