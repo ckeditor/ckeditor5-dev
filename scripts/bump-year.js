08a024ca78723122e2125a96500b60f0c82926b2
Przemyslaw Zan <p.zan@cksource.com>
2021-12-29 22:03:39 +0100

    Replaced old script execution with the new one.

--- scripts/bump-year.js
+++ scripts/bump-year.js
@@ -17,54 +17,18 @@ git commit -am "Internal: Bumped the year." && git push
 
 */
 
-const glob = require( 'glob' );
-const minimatch = require( 'minimatch' );
-const fs = require( 'fs' );
-
-const includeDotFiles = {
-	dot: true
-};
-
-glob( '!(node_modules|coverage|.nyc_output)/**', updateYear );
-
-// LICENSE.md, .eslintrc.js, etc.
-glob( '*', includeDotFiles, updateYear );
-
-function updateYear( err, fileNames ) {
-	const filteredFileNames = fileNames.filter( fileName => {
-		// Filter out stuff from packages' node_modules.
-		if ( minimatch( fileName, 'packages/**/node_modules/**' ) ) {
-			return false;
-		}
-
-		if ( fs.statSync( fileName ).isDirectory() ) {
-			return false;
-		}
-
-		return true;
-	} );
-
-	filteredFileNames.forEach( fileName => {
-		fs.readFile( fileName, ( err, data ) => {
-			data = data.toString();
-
-			const year = new Date().getFullYear();
-			const regexp = /Copyright \(c\) 2003-\d{4}/g;
-			const updatedData = data.replace( regexp, 'Copyright (c) 2003-' + year );
-
-			if ( data == updatedData ) {
-				// License headers are only required in JS files.
-				// Also, the file might have already been updated.
-				if ( fileName.endsWith( '.js' ) && !data.match( regexp ) ) {
-					console.warn( `The file "${ process.cwd() }/${ fileName }" misses a license header.` );
+require( '@ckeditor/ckeditor5-dev-env' )
+	.bumpYear( {
+		cwd: process.cwd(),
+		globPatterns: [
+			{ // LICENSE.md, .eslintrc.js, etc.
+				pattern: '*',
+				options: {
+					dot: true
 				}
-			} else {
-				fs.writeFile( fileName, updatedData, err => {
-					if ( err ) {
-						throw err;
-					}
-				} );
+			},
+			{
+				pattern: '!(node_modules|coverage|.nyc_output)/**'
 			}
-		} );
+		]
 	} );
-}
