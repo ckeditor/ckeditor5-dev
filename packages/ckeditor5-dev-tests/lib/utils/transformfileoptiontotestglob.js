2238034df7b58d7a0f1aa33cc90f7b40892f58c8
Przemyslaw Zan <p.zan@cksource.com>
2022-09-07 17:48:05 +0200

    Review requests.

--- packages/ckeditor5-dev-tests/lib/utils/transformfileoptiontotestglob.js
+++ packages/ckeditor5-dev-tests/lib/utils/transformfileoptiontotestglob.js
@@ -7,35 +7,26 @@
 
 const path = require( 'path' );
 
-// Matches pattern of a single package name, e.g. "engine", "special-characters".
-const SINGLE_PACKAGE_REGEXP = /^[a-z][a-z-]+[a-z]$/;
-// Matches pattern of a specific filename test path, e.g. "ckeditor5/article", "basic-styles/bold*".
-const NAMED_TEST_REGEXP = /^[a-z1-9-]+\/[*a-z-]+$/;
-// Matches pattern of excluded package names, e.g. "!engine", "!special-characters", "!(ui|core)".
-const EXCLUSION_REGEXP = /^!(?:\([a-z-|]+\)|[a-z-]+)$/;
-// Matches pattern of a single package name and a subdirectory, e.g. "engine/view/", "alignment/alignment/".
-const DIRECTORY_REGEXP = /^[a-z]+\/[/a-z-]+\/$/;
-
 /**
  * Converts values of --files argument to proper globs.
- * There are 5 supported types of values:
+ * These are the supported types of values:
  *
- * 0. "ckeditor5" - matches all root package tests
- * 1. "*"         - matches all packages' files
- * 2. "foo"       - matches all tests from a package
- * 3. "!foo"      - matches all tests except from a package
- * 4. "foo/bar/"  - matches all tests from a package and a subdirectory
- * 5. "foo/bar"   - matches all tests from a package (or root) with specific filename
+ * "ckeditor5" - matches all root package tests
+ * "*"         - matches all packages' files
+ * "foo"       - matches all tests from a package
+ * "!foo"      - matches all tests except from a package
+ * "foo/bar/"  - matches all tests from a package and a subdirectory
+ * "foo/bar"   - matches all tests from a package (or root) with specific filename
  *
- * @param {String} globPattern A path or pattern to determine the tests to execute.
+ * @param {String} pattern A path or pattern to determine the tests to execute.
  * @param {Boolean} [isManualTest=false] Whether the tests are manual or automated.
  * @returns {Iterable.<String>}
  */
-module.exports = function transformFileOptionToTestGlob( globPattern, isManualTest = false ) {
-	const transformedPath = transformSingleGlobPattern( globPattern, { isManualTest } );
-	const transformedPathWithCKEditorPrefix = transformSingleGlobPattern( globPattern, { isManualTest, useCKEditorPrefix: true } );
-	const transformedPathForExternalPackages = transformSingleGlobPattern( globPattern, { isManualTest, externalPackages: true } );
-	const transformedPathForExternalPackagesWithCKEditorPrefix = transformSingleGlobPattern( globPattern, {
+module.exports = function transformFileOptionToTestGlob( pattern, isManualTest = false ) {
+	const transformedPath = transformSinglePattern( pattern, { isManualTest } );
+	const transformedPathWithCKEditorPrefix = transformSinglePattern( pattern, { isManualTest, useCKEditorPrefix: true } );
+	const transformedPathForExternalPackages = transformSinglePattern( pattern, { isManualTest, externalPackages: true } );
+	const transformedPathForExternalPackagesWithCKEditorPrefix = transformSinglePattern( pattern, {
 		isManualTest,
 		externalPackages: true,
 		useCKEditorPrefix: true
@@ -58,67 +49,38 @@ module.exports = function transformFileOptionToTestGlob( globPattern, isManualTe
 };
 
 /**
- * @param {String} globPattern
+ * @param {String} pattern
  * @param {Object} [options={}]
  * @param {Boolean} [options.isManualTest=false] Whether the tests are manual or automated.
  * @param {Boolean} [options.useCKEditorPrefix=false] If true, the returned path will use 'ckeditor' prefix instead of 'ckeditor5'.
  * @param {Boolean} [options.externalPackages] If true, the returned path will contain "external\/**\/packages".
  * @returns {String}
  */
-function transformSingleGlobPattern( globPattern, options ) {
-	const rootTests = globPattern === 'ckeditor5' || globPattern.startsWith( 'ckeditor5/' );
+function transformSinglePattern( pattern, options ) {
+	const chunks = pattern.match( /[a-z1-9|*-]+/g );
+	const output = [];
+
+	const isExclusionPattern = pattern.startsWith( '!' );
+	const isFilenamePattern = pattern.includes( '/' ) && !pattern.endsWith( '/' );
+
 	const prefix = options.useCKEditorPrefix ? 'ckeditor' : 'ckeditor5';
 	const packagesDirectory = options.externalPackages ? [ 'external', '*', 'packages' ] : [ 'packages' ];
+	const packageName = isExclusionPattern ? `!(${ chunks.shift() })*` : chunks.shift();
+	const filename = isFilenamePattern ? chunks.pop() : '*';
 
-	const chunks = globPattern.match( /[a-z1-9|*-]+/g );
-	const returnChunks = [];
+	output.push( ...process.cwd().split( path.sep ) );
 
-	// Every path starts with workspace.
-	returnChunks.push( ...process.cwd().split( path.sep ) );
-
-	// We add path to packages directory, unless we operate in CKE5 root.
-	if ( !rootTests ) {
-		let packageName;
-		let testDirectories;
-
-		// 1, 2 & 5
-		if ( globPattern === '*' || SINGLE_PACKAGE_REGEXP.test( globPattern ) || NAMED_TEST_REGEXP.test( globPattern ) ) {
-			packageName = prefix + '-' + chunks[ 0 ];
-			testDirectories = [ '**' ];
-		}
-
-		// 3
-		if ( EXCLUSION_REGEXP.test( globPattern ) ) {
-			packageName = prefix + '-!(' + chunks[ 0 ] + ')*';
-			testDirectories = [ '**' ];
-		}
-
-		// 4
-		if ( DIRECTORY_REGEXP.test( globPattern ) ) {
-			packageName = prefix + '-' + chunks.shift();
-			testDirectories = [ ...chunks, '**' ];
-		}
-
-		returnChunks.push( ...packagesDirectory, packageName, 'tests', ...testDirectories );
-	}
-	// In case of CKE5 root, we simply append tests directory.
-	else {
-		returnChunks.push( 'tests', '**' );
+	if ( packageName !== 'ckeditor5' ) {
+		output.push( ...packagesDirectory, `${ prefix }-${ packageName }` );
 	}
 
-	// Subdirectory for manual tests.
+	output.push( 'tests' );
+
 	if ( options.isManualTest ) {
-		returnChunks.push( 'manual', '**' );
+		output.push( 'manual' );
 	}
 
-	// If we're looking for a single test, use specific filename.
-	if ( NAMED_TEST_REGEXP.test( globPattern ) ) {
-		returnChunks.push( chunks[ 1 ] + '.js' );
-	}
-	// Otherwise, any filename.
-	else {
-		returnChunks.push( '*.js' );
-	}
+	output.push( ...chunks, '**', `${ filename }.js` );
 
-	return returnChunks.join( path.posix.sep );
+	return output.join( path.posix.sep );
 }
--- packages/ckeditor5-dev-tests/tests/utils/transformfileoptiontotestglob.js
+++ packages/ckeditor5-dev-tests/tests/utils/transformfileoptiontotestglob.js
@@ -31,7 +31,7 @@ describe( 'dev-tests/utils', () => {
 		} );
 
 		it( 'for manual tests', () => {
-			expect( transformFileOptionToTestGlob( 'ckeditor5', true ) ).to.deep.equal( [ '/workspace/tests/**/manual/**/*.js' ] );
+			expect( transformFileOptionToTestGlob( 'ckeditor5', true ) ).to.deep.equal( [ '/workspace/tests/manual/**/*.js' ] );
 		} );
 	} );
 
@@ -47,16 +47,16 @@ describe( 'dev-tests/utils', () => {
 
 		it( 'for manual tests', () => {
 			expect( transformFileOptionToTestGlob( '*', true ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-*/tests/**/manual/**/*.js',
-				'/workspace/packages/ckeditor-*/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor5-*/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor-*/tests/**/manual/**/*.js'
+				'/workspace/packages/ckeditor5-*/tests/manual/**/*.js',
+				'/workspace/packages/ckeditor-*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor-*/tests/manual/**/*.js'
 			] );
 		} );
 	} );
 
 	describe( 'converts "foo" to pattern matching all tests from a package', () => {
-		it( 'for automated tests', () => {
+		it( 'for automated tests (single package)', () => {
 			expect( transformFileOptionToTestGlob( 'engine' ) ).to.deep.equal( [
 				'/workspace/packages/ckeditor5-engine/tests/**/*.js',
 				'/workspace/packages/ckeditor-engine/tests/**/*.js',
@@ -65,12 +65,30 @@ describe( 'dev-tests/utils', () => {
 			] );
 		} );
 
-		it( 'for manual tests', () => {
+		it( 'for automated tests (wildcard support)', () => {
+			expect( transformFileOptionToTestGlob( 'build-*' ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-build-*/tests/**/*.js',
+				'/workspace/packages/ckeditor-build-*/tests/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-build-*/tests/**/*.js',
+				'/workspace/external/*/packages/ckeditor-build-*/tests/**/*.js'
+			] );
+		} );
+
+		it( 'for manual tests (single package)', () => {
 			expect( transformFileOptionToTestGlob( 'engine', true ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-engine/tests/**/manual/**/*.js',
-				'/workspace/packages/ckeditor-engine/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor5-engine/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor-engine/tests/**/manual/**/*.js'
+				'/workspace/packages/ckeditor5-engine/tests/manual/**/*.js',
+				'/workspace/packages/ckeditor-engine/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-engine/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor-engine/tests/manual/**/*.js'
+			] );
+		} );
+
+		it( 'for manual tests (wildcard support)', () => {
+			expect( transformFileOptionToTestGlob( 'build-*', true ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-build-*/tests/manual/**/*.js',
+				'/workspace/packages/ckeditor-build-*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-build-*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor-build-*/tests/manual/**/*.js'
 			] );
 		} );
 	} );
@@ -96,39 +114,87 @@ describe( 'dev-tests/utils', () => {
 
 		it( 'for manual tests (single exclusion)', () => {
 			expect( transformFileOptionToTestGlob( '!engine', true ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-!(engine)*/tests/**/manual/**/*.js',
-				'/workspace/packages/ckeditor-!(engine)*/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor5-!(engine)*/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor-!(engine)*/tests/**/manual/**/*.js'
+				'/workspace/packages/ckeditor5-!(engine)*/tests/manual/**/*.js',
+				'/workspace/packages/ckeditor-!(engine)*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-!(engine)*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor-!(engine)*/tests/manual/**/*.js'
 			] );
 		} );
 
 		it( 'for manual tests (multiple exclusions)', () => {
 			expect( transformFileOptionToTestGlob( '!(engine|core|basic-styles)', true ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-!(engine|core|basic-styles)*/tests/**/manual/**/*.js',
-				'/workspace/packages/ckeditor-!(engine|core|basic-styles)*/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor5-!(engine|core|basic-styles)*/tests/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor-!(engine|core|basic-styles)*/tests/**/manual/**/*.js'
+				'/workspace/packages/ckeditor5-!(engine|core|basic-styles)*/tests/manual/**/*.js',
+				'/workspace/packages/ckeditor-!(engine|core|basic-styles)*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-!(engine|core|basic-styles)*/tests/manual/**/*.js',
+				'/workspace/external/*/packages/ckeditor-!(engine|core|basic-styles)*/tests/manual/**/*.js'
 			] );
 		} );
 	} );
 
-	describe( 'converts "foo/bar/" to pattern matching all tests from a package and a subdirectory', () => {
-		it( 'for automated tests', () => {
-			expect( transformFileOptionToTestGlob( 'engine/view/' ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-engine/tests/view/**/*.js',
-				'/workspace/packages/ckeditor-engine/tests/view/**/*.js',
-				'/workspace/external/*/packages/ckeditor5-engine/tests/view/**/*.js',
-				'/workspace/external/*/packages/ckeditor-engine/tests/view/**/*.js'
+	describe( 'converts "foo/bar/" to pattern matching all tests from a package (or root) and a subdirectory', () => {
+		it( 'for automated tests (root)', () => {
+			expect( transformFileOptionToTestGlob( 'ckeditor5/memory/' ) ).to.deep.equal( [
+				'/workspace/tests/memory/**/*.js'
 			] );
 		} );
 
-		it( 'for manual tests', () => {
-			expect( transformFileOptionToTestGlob( 'engine/view/', true ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-engine/tests/view/**/manual/**/*.js',
-				'/workspace/packages/ckeditor-engine/tests/view/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor5-engine/tests/view/**/manual/**/*.js',
-				'/workspace/external/*/packages/ckeditor-engine/tests/view/**/manual/**/*.js'
+		it( 'for automated tests (package)', () => {
+			expect( transformFileOptionToTestGlob( 'alignment/utils/' ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-alignment/tests/utils/**/*.js',
+				'/workspace/packages/ckeditor-alignment/tests/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-alignment/tests/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor-alignment/tests/utils/**/*.js'
+			] );
+		} );
+
+		it( 'for automated tests (wildcard support)', () => {
+			expect( transformFileOptionToTestGlob( 'basic-styles/bold*/' ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-basic-styles/tests/bold*/**/*.js',
+				'/workspace/packages/ckeditor-basic-styles/tests/bold*/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-basic-styles/tests/bold*/**/*.js',
+				'/workspace/external/*/packages/ckeditor-basic-styles/tests/bold*/**/*.js'
+			] );
+		} );
+
+		it( 'for automated tests (nested directories)', () => {
+			expect( transformFileOptionToTestGlob( 'core/editor/utils/' ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-core/tests/editor/utils/**/*.js',
+				'/workspace/packages/ckeditor-core/tests/editor/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-core/tests/editor/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor-core/tests/editor/utils/**/*.js'
+			] );
+		} );
+
+		it( 'for manual tests (root)', () => {
+			expect( transformFileOptionToTestGlob( 'ckeditor5/memory/', true ) ).to.deep.equal( [
+				'/workspace/tests/manual/memory/**/*.js'
+			] );
+		} );
+
+		it( 'for manual tests (package)', () => {
+			expect( transformFileOptionToTestGlob( 'alignment/utils/', true ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-alignment/tests/manual/utils/**/*.js',
+				'/workspace/packages/ckeditor-alignment/tests/manual/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-alignment/tests/manual/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor-alignment/tests/manual/utils/**/*.js'
+			] );
+		} );
+
+		it( 'for manual tests (wildcard support)', () => {
+			expect( transformFileOptionToTestGlob( 'basic-styles/bold*/', true ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-basic-styles/tests/manual/bold*/**/*.js',
+				'/workspace/packages/ckeditor-basic-styles/tests/manual/bold*/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-basic-styles/tests/manual/bold*/**/*.js',
+				'/workspace/external/*/packages/ckeditor-basic-styles/tests/manual/bold*/**/*.js'
+			] );
+		} );
+
+		it( 'for manual tests (nested directories)', () => {
+			expect( transformFileOptionToTestGlob( 'core/editor/utils/', true ) ).to.deep.equal( [
+				'/workspace/packages/ckeditor5-core/tests/manual/editor/utils/**/*.js',
+				'/workspace/packages/ckeditor-core/tests/manual/editor/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor5-core/tests/manual/editor/utils/**/*.js',
+				'/workspace/external/*/packages/ckeditor-core/tests/manual/editor/utils/**/*.js'
 			] );
 		} );
 	} );
@@ -149,7 +215,7 @@ describe( 'dev-tests/utils', () => {
 			] );
 		} );
 
-		it( 'for automated tests (wildcard filename)', () => {
+		it( 'for automated tests (wildcard support)', () => {
 			expect( transformFileOptionToTestGlob( 'basic-styles/bold*' ) ).to.deep.equal( [
 				'/workspace/packages/ckeditor5-basic-styles/tests/**/bold*.js',
 				'/workspace/packages/ckeditor-basic-styles/tests/**/bold*.js',
@@ -160,25 +226,25 @@ describe( 'dev-tests/utils', () => {
 
 		it( 'for manual tests (root)', () => {
 			expect( transformFileOptionToTestGlob( 'ckeditor5/utils', true ) ).to.deep.equal( [
-				'/workspace/tests/**/manual/**/utils.js'
+				'/workspace/tests/manual/**/utils.js'
 			] );
 		} );
 
 		it( 'for manual tests (package)', () => {
 			expect( transformFileOptionToTestGlob( 'alignment/utils', true ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-alignment/tests/**/manual/**/utils.js',
-				'/workspace/packages/ckeditor-alignment/tests/**/manual/**/utils.js',
-				'/workspace/external/*/packages/ckeditor5-alignment/tests/**/manual/**/utils.js',
-				'/workspace/external/*/packages/ckeditor-alignment/tests/**/manual/**/utils.js'
+				'/workspace/packages/ckeditor5-alignment/tests/manual/**/utils.js',
+				'/workspace/packages/ckeditor-alignment/tests/manual/**/utils.js',
+				'/workspace/external/*/packages/ckeditor5-alignment/tests/manual/**/utils.js',
+				'/workspace/external/*/packages/ckeditor-alignment/tests/manual/**/utils.js'
 			] );
 		} );
 
-		it( 'for manual tests (wildcard filename)', () => {
+		it( 'for manual tests (wildcard support)', () => {
 			expect( transformFileOptionToTestGlob( 'basic-styles/bold*', true ) ).to.deep.equal( [
-				'/workspace/packages/ckeditor5-basic-styles/tests/**/manual/**/bold*.js',
-				'/workspace/packages/ckeditor-basic-styles/tests/**/manual/**/bold*.js',
-				'/workspace/external/*/packages/ckeditor5-basic-styles/tests/**/manual/**/bold*.js',
-				'/workspace/external/*/packages/ckeditor-basic-styles/tests/**/manual/**/bold*.js'
+				'/workspace/packages/ckeditor5-basic-styles/tests/manual/**/bold*.js',
+				'/workspace/packages/ckeditor-basic-styles/tests/manual/**/bold*.js',
+				'/workspace/external/*/packages/ckeditor5-basic-styles/tests/manual/**/bold*.js',
+				'/workspace/external/*/packages/ckeditor-basic-styles/tests/manual/**/bold*.js'
 			] );
 		} );
 	} );
