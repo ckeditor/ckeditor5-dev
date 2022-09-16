aee3f9498bd0b7bf358b5f10228ef2199504716c
Kamil Piechaczek <pomek@users.noreply.github.com>
2022-09-16 15:44:11 +0200

    The working implementation of the typedoc-plugin-module-fixer plugin.

--- packages/ckeditor5-dev-docs/lib/docs/plugins/typedoc-plugin-module-fixer.js
+++ packages/ckeditor5-dev-docs/lib/docs/plugins/typedoc-plugin-module-fixer.js
@@ -7,16 +7,50 @@
 
 const { Converter, ReflectionKind } = require( 'typedoc' );
 
+/**
+ * The `typedoc-plugin-module-fixer` reads the module name specified in the `@module` tag name.
+ *
+ *      @module package/file
+ *
+ * For the example specified above, a name of the parsed module should be equal to "package/file".
+ *
+ * It may happen that imports statement are specified above the "@module" block code.
+ * In such a case, built-in plugin in `typedoc` does not read its value properly.
+ */
 module.exports = {
 	load( app ) {
 		app.converter.on(
 			Converter.EVENT_CREATE_DECLARATION,
-			function( _context, reflection ) {
-				if ( reflection.kind === ReflectionKind.Module && reflection.name.endsWith( 'inserttopriorityarray' ) ) {
-					// console.log( node.externalModuleIndicator );
-					// console.log( Object.keys(reflection) );
-					// console.log( reflection.sources[0].file.reflections );
-					// console.log( require( 'util' ).inspect( reflection, { showHidden: false, depth: 1, colors: true } ) );
+			function( _context, reflection, node ) {
+				if ( reflection.kind !== ReflectionKind.Module ) {
+					return;
+				}
+
+				// Iterate over statements...
+				for ( const statement of node.statements ) {
+					if ( !Array.isArray( statement.jsDoc ) ) {
+						continue;
+					}
+
+					// ...to find a JSDoc block code...
+					for ( const jsDoc of statement.jsDoc ) {
+						// ...that represents a module definition.
+						const [ moduleTag ] = ( jsDoc.tags || [] ).filter( tag => {
+							return tag.tagName.originalKeywordKind === 141;
+						} );
+
+						if ( !moduleTag ) {
+							continue;
+						}
+
+						// When found, use its value as a module name.
+						if ( reflection.name !== moduleTag.comment ) {
+							reflection.originalName = reflection.name;
+							reflection.name = moduleTag.comment;
+
+							return;
+						}
+					}
 				}
 			}
 		);
