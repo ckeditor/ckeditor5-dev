8e2ff4deff3800fe43a7a9624ff5f45a4414abc3
Marcin Panek <m.panek@cksource.com>
2023-02-13 09:07:43 +0100

    Prevent validating inherited classes.

---
+++ packages/ckeditor5-dev-docs/tests/validators/link-validator/fixtures/inheritance/A.ts
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/**
+ * @module fixtures/inheritance/A
+ */
+
+export default class A {
+	/**
+	 * {@link ~C} This is a link to non-existing class that throws an error.
+	 */
+	public inherited: string = 'test';
+}
---
+++ packages/ckeditor5-dev-docs/tests/validators/link-validator/fixtures/inheritance/B.ts
@@ -0,0 +1,12 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import A from './A';
+
+/**
+ * @module fixtures/inheritance/B
+ */
+
+export default class B extends A {}
