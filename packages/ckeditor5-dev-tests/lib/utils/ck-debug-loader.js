d2a07b2a6de9e2c11ba50487760b64a64168b7a6
Marcin Panek <m.panek@cksource.com>
2023-04-05 13:28:52 +0200

    Revert "Update `ckDebugLoader` to hanle `-- @preserve` option for esbuild."
    This reverts commit f0953b2a777c3178b45bd54f2d157fc9234a6c20.

--- packages/ckeditor5-dev-tests/lib/utils/ck-debug-loader.js
+++ packages/ckeditor5-dev-tests/lib/utils/ck-debug-loader.js
@@ -13,7 +13,7 @@
  * @param {any} map
  */
 module.exports = function ckDebugLoader( source, map ) {
-	source = source.replace( /\/\/ @if (-- @preserve )?(!?[\w]+) \/\/(.+)/g, ( match, preserve, flagName, body ) => {
+	source = source.replace( /\/\/ @if (!?[\w]+) \/\/(.+)/g, ( match, flagName, body ) => {
 		// `this.query` comes from the webpack loader configuration specified as the loader options.
 		// {
 		//   loader: 'path-to-the-file',
@@ -28,9 +28,7 @@ module.exports = function ckDebugLoader( source, map ) {
 		}
 
 		// Uncomment the code with a same length string to not break the source maps.
-		return preserve ?
-			`/* @if ${ preserve }${ flagName } */ ${ body }` :
-			`/* @if ${ flagName } */ ${ body }`;
+		return `/* @if ${ flagName } */ ${ body }`;
 	} );
 
 	this.callback( null, source, map );
