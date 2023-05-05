2a13d2c417d6e51a29de6fdd6103bb85aca81d9b
Marcin Panek <m.panek@cksource.com>
2023-05-05 17:26:30 +0200

    Addressed PR comments.

--- packages/ckeditor5-dev-release-tools/lib/release/updateversions.js
+++ packages/ckeditor5-dev-release-tools/lib/release/updateversions.js
@@ -13,27 +13,30 @@ const { tools } = require( '@ckeditor/ckeditor5-dev-utils' );
 const getPackageJson = require( '../utils/getpackagejson' );
 
 /**
- * Updates version of root package and subpackages if `packagesDirectory` path is provided.
+ * The purpose of the script is to update the version of a root package found in the current working
+ * directory and packages if the `options.packagesDirectory` path is provided.
  *
  * @param {Object} options
- * @param {String} options.version
- * @param {String} [options.packagesDirectory]
- * @param {String} [options.cwd]
+ * @param {String} options.version Version to store in a `package.json` file under the `version` key.
+ * @param {String} [options.packagesDirectory] Relative path to a location of packages to update. If not specified,
+ * only the root package is checked.
+ * @param {String} [options.cwd=process.cwd()] Current working directory from which all paths will be resolved.
  */
 module.exports = function updateVersions( { packagesDirectory, version, cwd = process.cwd() } ) {
-	const normCwd = toUnix( cwd );
-	const normPackagesDir = packagesDirectory ? normalizeTrim( packagesDirectory ) : packagesDirectory;
-	const packagesGlobs = normPackagesDir ? [ normPackagesDir + '/*', './' ] : './';
-	const packagesPaths = globSync( packagesGlobs, { cwd: normCwd, absolute: true } );
+	const normalizedCwd = toUnix( cwd );
+	const normalizedPackagesDir = packagesDirectory ? normalizeTrim( packagesDirectory ) : null;
+	const packagesGlobs = normalizedPackagesDir ? [ normalizedPackagesDir + '/*', './' ] : './';
+	const packagesPaths = globSync( packagesGlobs, { cwd: normalizedCwd, absolute: true } );
 
-	checkVersionGreaterThanOldVersion( version, getPackageJson( normCwd ).version );
-	checkVersionIsAvailableInNpm( version, getRandomPackageJson( normPackagesDir, normCwd ).name );
+	checkVersionGreaterThanOldVersion( version, getPackageJson( normalizedCwd ).version );
+	checkVersionIsAvailableInNpm( version, getRandomPackageJson( normalizedPackagesDir, normalizedCwd ).name );
 
 	for ( const packagePath of packagesPaths ) {
 		const packageJsonPath = join( packagePath, 'package.json' );
 		const packageJson = getPackageJson( packagePath );
 
-		fs.outputJsonSync( packageJsonPath, { ...packageJson, version }, { spaces: 2 } );
+		packageJson.version = version;
+		fs.outputJsonSync( packageJsonPath, packageJson, { spaces: 2 } );
 	}
 };
 
@@ -46,20 +49,21 @@ function getRandomPackageJson( packagesDirectory, cwd ) {
 	const globs = packagesDirectory ? packagesDirectory + '/*' : './';
 	const packagesPaths = globSync( globs, { cwd, absolute: true } );
 	const randomPackagePath = packagesPaths[ Math.floor( Math.random() * packagesPaths.length ) ];
+
 	return getPackageJson( randomPackagePath );
 }
 
 /**
  * @param {String} newVersion
- * @param {String} oldVersion
+ * @param {String} currentVersion
  */
-function checkVersionGreaterThanOldVersion( newVersion, oldVersion ) {
+function checkVersionGreaterThanOldVersion( newVersion, currentVersion ) {
 	if ( !semver.valid( newVersion ) ) {
 		throw new Error( `Provided version ${ newVersion } must be a valid semver version.` );
 	}
 
-	if ( !semver.gt( newVersion, oldVersion ) ) {
-		throw new Error( `Provided version ${ newVersion } must be greater than ${ oldVersion }.` );
+	if ( !semver.gt( newVersion, currentVersion ) ) {
+		throw new Error( `Provided version ${ newVersion } must be greater than ${ currentVersion }.` );
 	}
 }
 
