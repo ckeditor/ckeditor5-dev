7e658e8764b4d3c37e62ccfc7f608992d661e4eb
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-05-18 11:18:36 +0200

    createGithubRelease() will not create the same release twice.

--- packages/ckeditor5-dev-release-tools/lib/tasks/creategithubrelease.js
+++ packages/ckeditor5-dev-release-tools/lib/tasks/creategithubrelease.js
@@ -6,9 +6,10 @@
 'use strict';
 
 const { Octokit } = require( '@octokit/rest' );
+const semver = require( 'semver' );
 
 /**
- * Create a Github release.
+ * Create a GitHub release.
  *
  * @param {Object} options
  * @param {String} options.token Token used to authenticate with GitHub.
@@ -18,7 +19,7 @@ const { Octokit } = require( '@octokit/rest' );
  * @param {String} options.description Description of the release.
  * @returns {Promise.<String>}
  */
-module.exports = function createGithubRelease( options ) {
+module.exports = async function createGithubRelease( options ) {
 	const {
 		token,
 		version,
@@ -27,25 +28,77 @@ module.exports = function createGithubRelease( options ) {
 		description
 	} = options;
 
-	const isPrerelease = false; // TODO: Detect it from a version.
-
 	const github = new Octokit( {
 		version: '3.0.0',
 		auth: `token ${ token }`
 	} );
 
-	const releaseParams = {
-		tag_name: `v${ version }`,
+	if ( await shouldCreateRelease( github, repositoryOwner, repositoryName, version ) ) {
+		await github.repos.createRelease( {
+			tag_name: `v${ version }`,
+			owner: repositoryOwner,
+			repo: repositoryName,
+			body: description,
+			prerelease: getVersionTag( version ) !== 'latest'
+		} );
+	}
+
+	return `https://github.com/${ repositoryOwner }/${ repositoryName }/releases/tag/v${ version }`;
+};
+
+/**
+ * Returns an npm tag based on the specified release version.
+ *
+ * @param {String} version
+ * @returns {String}
+ */
+function getVersionTag( version ) {
+	const [ versionTag ] = semver.prerelease( version ) || [ 'latest' ];
+
+	return versionTag;
+}
+
+/**
+ * Resolves a promise containing a flag if the GitHub contains the release page for given version.
+ *
+ * @param {Octokit} github
+ * @param {String} repositoryOwner
+ * @param {String} repositoryName
+ * @param {String} version
+ * @returns {Promise.<boolean>}
+ */
+async function shouldCreateRelease( github, repositoryOwner, repositoryName, version ) {
+	const releaseDetails = await getLastRelease( github, repositoryOwner, repositoryName );
+
+	// It can be `null` if there is no releases on GitHub.
+	let githubVersion = releaseDetails.data.tag_name;
+
+	if ( githubVersion ) {
+		githubVersion = releaseDetails.data.tag_name.replace( /^v/, '' );
+	}
+
+	// If versions are different, we are ready to create a new release.
+	return githubVersion !== version;
+}
+
+function getLastRelease( github, repositoryOwner, repositoryName ) {
+	const requestParams = {
 		owner: repositoryOwner,
-		repo: repositoryName,
-		body: description,
-		prerelease: isPrerelease
+		repo: repositoryName
 	};
 
-	// TODO: Tests.
+	return github.repos.getLatestRelease( requestParams )
+		.catch( err => {
+			// If the "last release" returned the 404 error page, it means that this release
+			// will be the first one for specified `repositoryOwner/repositoryName` package.
+			if ( err.status == 404 ) {
+				return Promise.resolve( {
+					data: {
+						tag_name: null
+					}
+				} );
+			}
 
-	return github.repos.createRelease( releaseParams )
-		.then( () => {
-			return `https://github.com/${ repositoryOwner }/${ repositoryName }/releases/tag/v${ version }`;
+			return Promise.reject( err );
 		} );
-};
+}
