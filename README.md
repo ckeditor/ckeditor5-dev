42e1e51778d06621d6d615bd9babbea84949d645
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-05-22 15:38:11 +0200

    Internal: Integraded the new release tools.

--- README.md
+++ README.md
@@ -72,10 +72,8 @@ After generating the changelog, you are able to release the package.
 First, you need to bump the version:
 
 ```bash
-npm run release:bump-version
+npm run release:prepare-packages
 ```
-
-You can also use the `--dry-run` option in order to see what this task does.
 
 After bumping the version, you can publish the changes:
 
@@ -83,9 +81,7 @@ After bumping the version, you can publish the changes:
 npm run release:publish
 ```
 
-As in the previous task, the `--dry-run` option is also available.
-
-Your job's done. You can go now to `ckeditor5`, remove `yarn.lock`, potentially update something in `package.json`, run `yarn install` and commit that as `"Internal: Updated dependencies."`.
+Your job's done. You can go now to `ckeditor5`, remove `yarn.lock`, potentially update entries in `package.json`, run `yarn install` and commit that as `"Internal: Updated dependencies."`.
 
 ## License
 
---
+++ scripts/utils/parsearguments.js
@@ -0,0 +1,54 @@
+/**
+ * @license Copyright (c) 2003-2023, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* eslint-env node */
+
+'use strict';
+
+const minimist = require( 'minimist' );
+
+/**
+ * @param {Array.<String>} cliArguments
+ * @returns {ReleaseOptions} options
+ */
+module.exports = function parseArguments( cliArguments ) {
+	const config = {
+		number: [
+			'concurrency'
+		],
+
+		string: [
+			'packages',
+			'npm-tag'
+		],
+
+		default: {
+			concurrency: require( 'os' ).cpus().length / 2,
+			packages: null,
+			'npm-tag': 'staging'
+		}
+	};
+
+	const options = minimist( cliArguments, config );
+
+	if ( typeof options.packages === 'string' ) {
+		options.packages = options.packages.split( ',' );
+	}
+
+	options.npmTag = options[ 'npm-tag' ];
+	delete options[ 'npm-tag' ];
+
+	return options;
+};
+
+/**
+ * @typedef {Object} ReleaseOptions
+ *
+ * @property {Number} concurrency
+ *
+ * @property {String} [npmTag='staging']
+ *
+ * @property {Array.<String>|null} packages
+ */
