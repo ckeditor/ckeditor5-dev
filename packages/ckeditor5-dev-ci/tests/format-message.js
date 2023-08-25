ce26b10164711dd4dc3d3f3eeea95edbb4107ea0
Kamil Piechaczek <pomek@users.noreply.github.com>
2023-08-25 21:12:34 +0200

    Formatter should find a Slack account based on a GitHub account case-insensitive.

--- packages/ckeditor5-dev-ci/tests/format-message.js
+++ packages/ckeditor5-dev-ci/tests/format-message.js
@@ -20,7 +20,10 @@ describe( 'lib/format-message', () => {
 		};
 
 		formatMessage = proxyquire( '../lib/format-message', {
-			'node-fetch': stubs.nodeFetch
+			'node-fetch': stubs.nodeFetch,
+			'./data/members.json': {
+				ExampleNick: 'slackId'
+			}
 		} );
 	} );
 
@@ -140,6 +143,45 @@ describe( 'lib/format-message', () => {
 			expect( message ).to.be.an( 'object' );
 			expect( message ).to.have.property( 'text' );
 			expect( message.text ).to.equal( '<!channel> (CKEditor5DevopsBot), could you take a look?' );
+		} );
+
+		it( 'should find a Slack account based on a GitHub account case-insensitive', async () => {
+			stubs.nodeFetch.resolves( {
+				json() {
+					return Promise.resolve( {
+						author: {
+							login: 'exampleNICK'
+						},
+						commit: {
+							author: {
+								name: 'Example Nick'
+							},
+							message: 'An example message.'
+						},
+						sha: '35cbea88dc0b5c00406c9a5f0c357ad2a7195a19'
+					} );
+				}
+			} );
+
+			const message = await formatMessage( {
+				slackMessageUsername: 'Test',
+				iconUrl: 'https://avatars.githubusercontent.com/u/26329082?v=4',
+				repositoryOwner: 'ckeditor',
+				repositoryName: 'ckeditor5-dev',
+				branch: 'master',
+				buildTitle: 'Workflow',
+				buildUrl: 'https://...',
+				buildId: 1,
+				githubToken: 'secret-token',
+				triggeringCommitUrl: 'https://github.com/ckeditor/ckeditor5-dev/commit/35cbea88dc0b5c00406c9a5f0c357ad2a7195a19',
+				startTime: 1,
+				endTime: 2,
+				shouldHideAuthor: false
+			} );
+
+			expect( message ).to.be.an( 'object' );
+			expect( message ).to.have.property( 'text' );
+			expect( message.text ).to.equal( '<@slackId>, could you take a look?' );
 		} );
 	} );
 } );
