621d88e7b24097897cfafba088117fc3a2fd737b
Paweł Smyrek <p.smyrek@cksource.com>
2023-12-05 14:51:09 +0100

    Wrapped tests in own sections.

--- packages/ckeditor5-dev-stale-bot/tests/utils/preparesearchquery.js
+++ packages/ckeditor5-dev-stale-bot/tests/utils/preparesearchquery.js
@@ -6,59 +6,61 @@
 const expect = require( 'chai' ).expect;
 const prepareSearchQuery = require( '../../lib/utils/preparesearchquery' );
 
-describe( 'lib/utils/preparesearchquery', () => {
-	it( 'should be a function', () => {
-		expect( prepareSearchQuery ).to.be.a( 'function' );
-	} );
+describe( 'dev-stale-bot/lib/utils', () => {
+	describe( 'prepareSearchQuery', () => {
+		it( 'should be a function', () => {
+			expect( prepareSearchQuery ).to.be.a( 'function' );
+		} );
 
-	it( 'should prepare a query with repository slug', () => {
-		expect( prepareSearchQuery( { repositorySlug: 'ckeditor/ckeditor5' } ) ).to.include( 'repo:ckeditor/ckeditor5' );
-	} );
+		it( 'should prepare a query with repository slug', () => {
+			expect( prepareSearchQuery( { repositorySlug: 'ckeditor/ckeditor5' } ) ).to.include( 'repo:ckeditor/ckeditor5' );
+		} );
 
-	it( 'should prepare a query for issue', () => {
-		expect( prepareSearchQuery( { type: 'issue' } ) ).to.include( 'type:issue' );
-	} );
+		it( 'should prepare a query for issue', () => {
+			expect( prepareSearchQuery( { type: 'issue' } ) ).to.include( 'type:issue' );
+		} );
 
-	it( 'should prepare a query for pull request', () => {
-		expect( prepareSearchQuery( { type: 'pr' } ) ).to.include( 'type:pr' );
-	} );
+		it( 'should prepare a query for pull request', () => {
+			expect( prepareSearchQuery( { type: 'pr' } ) ).to.include( 'type:pr' );
+		} );
 
-	it( 'should prepare a query from specified date', () => {
-		expect( prepareSearchQuery( { searchDate: '2022-12-01' } ) ).to.include( 'created:<2022-12-01' );
-	} );
+		it( 'should prepare a query from specified date', () => {
+			expect( prepareSearchQuery( { searchDate: '2022-12-01' } ) ).to.include( 'created:<2022-12-01' );
+		} );
 
-	it( 'should prepare a query for open items', () => {
-		expect( prepareSearchQuery( {} ) ).to.include( 'state:open' );
-	} );
+		it( 'should prepare a query for open items', () => {
+			expect( prepareSearchQuery( {} ) ).to.include( 'state:open' );
+		} );
 
-	it( 'should prepare a query sorted in descending order by creation date', () => {
-		expect( prepareSearchQuery( {} ) ).to.include( 'sort:created-desc' );
-	} );
+		it( 'should prepare a query sorted in descending order by creation date', () => {
+			expect( prepareSearchQuery( {} ) ).to.include( 'sort:created-desc' );
+		} );
 
-	it( 'should prepare a query with ignored labels', () => {
-		const ignoredLabels = [
-			'status:stale',
-			'support:1',
-			'support:2',
-			'support:3',
-			'domain:accessibility'
-		];
+		it( 'should prepare a query with ignored labels', () => {
+			const ignoredLabels = [
+				'status:stale',
+				'support:1',
+				'support:2',
+				'support:3',
+				'domain:accessibility'
+			];
 
-		expect( prepareSearchQuery( { ignoredLabels } ) ).to.include(
-			'-label:status:stale -label:support:1 -label:support:2 -label:support:3 -label:domain:accessibility'
-		);
-	} );
+			expect( prepareSearchQuery( { ignoredLabels } ) ).to.include(
+				'-label:status:stale -label:support:1 -label:support:2 -label:support:3 -label:domain:accessibility'
+			);
+		} );
 
-	it( 'should prepare a query with all fields separated by space', () => {
-		const options = {
-			type: 'issue',
-			repositorySlug: 'ckeditor/ckeditor5',
-			searchDate: '2022-12-01',
-			ignoredLabels: [ 'status:stale' ]
-		};
+		it( 'should prepare a query with all fields separated by space', () => {
+			const options = {
+				type: 'issue',
+				repositorySlug: 'ckeditor/ckeditor5',
+				searchDate: '2022-12-01',
+				ignoredLabels: [ 'status:stale' ]
+			};
 
-		expect( prepareSearchQuery( options ) ).to.include(
-			'repo:ckeditor/ckeditor5 created:<2022-12-01 type:issue state:open sort:created-desc -label:status:stale'
-		);
+			expect( prepareSearchQuery( options ) ).to.include(
+				'repo:ckeditor/ckeditor5 created:<2022-12-01 type:issue state:open sort:created-desc -label:status:stale'
+			);
+		} );
 	} );
 } );
