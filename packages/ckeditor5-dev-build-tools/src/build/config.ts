ebdc0bc079ef1b3b2f41add3bdb72638087cb8aa
Filip Sobol <filipsobol@users.noreply.github.com>
2024-02-02 16:26:48 +0100

    Use TypeScript plugin in Rollup only when tsconfig file exists

--- packages/ckeditor5-dev-build-tools/src/build/config.ts
+++ packages/ckeditor5-dev-build-tools/src/build/config.ts
@@ -1,8 +1,12 @@
-import { readFileSync } from 'fs';
 import { createRequire } from 'module';
-import { defineConfig, type RollupOptions } from 'rollup';
-import { PackageJson } from 'type-fest';
+import { readFileSync, accessSync, constants } from 'fs';
+import chalk from 'chalk';
+import { defineConfig, Plugin, type RollupOptions } from 'rollup';
+import type { PackageJson } from 'type-fest';
 
+/**
+ * Rollup plugins
+ */
 import json from '@rollup/plugin-json';
 import styles from 'rollup-plugin-styles';
 // import modify from 'rollup-plugin-modify';
@@ -12,6 +16,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescriptPlugin from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
+/**
+ * PostCSS plugins
+ */
 import postcssImport from 'postcss-import';
 import postcssMixins from 'postcss-mixins';
 import postcssNesting from 'postcss-nesting';
@@ -81,12 +88,6 @@ async function getConfiguration( {
 	bundle,
 	minify
 }: Options ): Promise<RollupOptions> {
-	const require = createRequire( import.meta.url );
-	const typescriptPath = require.resolve(
-		'typescript',
-		{ paths: [ process.cwd() ] }
-	);
-
 	return defineConfig( {
 		input,
 
@@ -97,54 +98,67 @@ async function getConfiguration( {
 		external: ( id: string ) => !bundle && externals.some( name => id.startsWith( name ) ),
 
 		plugins: [
+			/**
+			 * Converts CommonJS modules to ES6.
+			 */
 			// @ts-ignore
 			commonjs( {
 				sourceMap,
 				defaultIsModuleExports: true
 			} ),
 
+			/**
+			 * Resolves imports using the Node resolution algorithm.
+			 */
 			nodeResolve( {
 				browser: true,
 				preferBuiltins: false
 			} ),
 
+			/**
+			 * Allows importing JSON files.
+			 */
 			// @ts-ignore
 			json(),
 
+			/**
+			 * Turns SVG file imports into JavaScript strings.
+			 */
 			svg( {
 				stringify: true
 			} ),
 
+			/**
+			 * Builds translation from the `.po` files.
+			 */
 			translations && translationsPlugin(),
 
+			/**
+			 * Allows using imports, mixins and nesting in CSS and exctacts output CSS to a separate file.
+			 */
 			// @ts-ignore
 			styles( {
 				mode: [
 					'extract',
-					browser ? 'styles.min.css' : 'styles.css'
+					minify ? 'styles.min.css' : 'styles.css'
 				],
 				plugins: [
 					postcssImport,
 					postcssMixins,
 					postcssNesting
 				],
-				minimize: browser,
+				minimize: minify,
 				sourceMap
 			} ),
 
-			// @ts-ignore
-			typescriptPlugin( {
-				tsconfig,
-				sourceMap,
-				typescript: require( typescriptPath ),
-				declaration: !browser,
-				declarationDir: !browser ? getPath( 'dist', 'types' ) : undefined,
-				declarationMap: false, // TODO: Do we need this?
-				compilerOptions: {
-					rootDir: !browser ? getPath( 'src' ) : undefined,
-				}
-			} ),
+			/**
+			 * Adds support for TypeScript syntax if tsconfig file exists.
+			 */
+			getTypeScriptPlugin( { tsconfig, sourceMap, browser } ),
 
+			/**
+			 * Minifies and mangles the output. It also removes all code comments except for license comments.
+			 */
 			// @ts-ignore
 			minify && terser( {
 				sourceMap,
@@ -155,4 +169,46 @@ async function getConfiguration( {
 			} )
 		]
 	} );
+}
+
+/**
+ * Returns the TypeScript plugin if tsconfig file exists, otherwise doesn't return anything.
+ */
+function getTypeScriptPlugin( {
+	tsconfig,
+	sourceMap,
+	browser
+}: Pick<Options, 'tsconfig' | 'sourceMap' | 'browser'> ): Plugin | void {
+	try {
+		/**
+		 * Check if tsconfig file exists.
+		 */
+		accessSync( tsconfig, constants.R_OK );
+
+		/**
+		 * Get the path to TypeScript relative to the current working directory. This is needed,
+		 * because this plugin might use different TypeScript version than the project using it.
+		 */
+		const require = createRequire( import.meta.url );
+		const typescriptPath = require.resolve(
+			'typescript',
+			{ paths: [ process.cwd() ] }
+		);
+
+		// @ts-ignore
+		return typescriptPlugin( {
+			tsconfig,
+			sourceMap,
+			typescript: require( typescriptPath ),
+			declaration: !browser,
+			declarationDir: !browser ? getPath( 'dist', 'types' ) : undefined,
+			declarationMap: false, // TODO: Do we need this?
+			compilerOptions: {
+				rootDir: !browser ? getPath( 'src' ) : undefined,
+			}
+		} );
+	} catch {
+		console.log( chalk.yellow( 'Could not find the TypeScript configuration file. Skipping TypeScript processing.\n' ) );
+		return;
+	}
 }
