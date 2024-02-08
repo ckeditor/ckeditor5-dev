25c4f685b9abdbcd2f34c081d4f844ae7b4439a7
Filip Sobol <filipsobol@users.noreply.github.com>
2024-02-08 16:38:45 +0100

    Move camelizing functions to utils

--- packages/ckeditor5-dev-build-tools/src/build/cli.ts
+++ packages/ckeditor5-dev-build-tools/src/build/cli.ts
@@ -1,27 +1,8 @@
 import { rmSync } from 'fs';
-import { parseArgs, type ParseArgsConfig } from 'util';
+import { parseArgs } from 'util';
 import { rollup } from 'rollup';
-import type { CamelCase, CamelCasedProperties } from 'type-fest';
-import { getPath } from '../utils.js';
+import { getPath, camelizeObjectKeys } from '../utils.js';
 import { getRollupOutputs, type Options } from './config.js';
-
-/**
- * Transforms `kebab-case` strings to `camelCase`.
- */
-function camelize<const T extends string>( s: T ): CamelCase<T> {
-	return s.replace( /-./g, x => x[1]!.toUpperCase() ) as CamelCase<T>;
-}
-
-/**
- * Transforms all object keys from `kebab-case` to `camelCase`.
- */
-function camelizeObjectKeys<const T extends Record<string, any>>( obj: T ): CamelCasedProperties<T> {
-	return Object.fromEntries(
-		Object
-			.entries( obj )
-			.map( ( [ key, value ] ) => [ camelize( key ), value ] )
-	) as CamelCasedProperties<T>;
-}
 
 /**
  * Banner added to the top of output JavaScript files.
@@ -32,59 +13,60 @@ const banner: string = `
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */`;
 
-
 export async function buildProject(): Promise<void> {
-	const options = {
-		'input': {
-			type: 'string',
-			default: 'src/index.ts'
-		},
-		'tsconfig': {
-			type: 'string',
-			default: 'tsconfig.json'
-		},
-		'browser': {
-			type: 'boolean',
-			default: false
-		},
-		'translations': {
-			type: 'boolean',
-			default: false
-		},
-		'source-map': {
-			type: 'boolean',
-			default: false
-		},
-		'bundle': {
-			type: 'boolean',
-			default: false
-		},
-		'external': {
-			type: 'string',
-			multiple: true,
-			default: []
-		},
-		'minify': {
-			type: 'boolean',
-			default: false
-		},
-		'clean': {
-			type: 'boolean',
-			default: false
-		}
-	} satisfies ParseArgsConfig['options'];
-
 	/**
 	 * Parse raw CLI arguments.
 	 */
 	const { values } = parseArgs( {
-		options,
+		options: {
+			'input': {
+				type: 'string',
+				default: 'src/index.ts'
+			},
+			'tsconfig': {
+				type: 'string',
+				default: 'tsconfig.json'
+			},
+			'browser': {
+				type: 'boolean',
+				default: false
+			},
+			'translations': {
+				type: 'boolean',
+				default: false
+			},
+			'source-map': {
+				type: 'boolean',
+				default: false
+			},
+			'bundle': {
+				type: 'boolean',
+				default: false
+			},
+			'external': {
+				type: 'string',
+				multiple: true,
+				default: []
+			},
+			'minify': {
+				type: 'boolean',
+				default: false
+			},
+			'clean': {
+				type: 'boolean',
+				default: false
+			}
+		},
+
+		// Skip `node` and the name of the command.
 		args: process.argv.slice(2),
+
+		// Fail when unknown argument is used.
 		strict: true
 	} );
 
 	/**
-	 * Transform `kebab-case` object keys to `camelCase`.
+	 * Transform `kebab-case` argument keys into `camelCase`.
 	 */
 	const args = camelizeObjectKeys( values );
 
--- packages/ckeditor5-dev-build-tools/src/utils.ts
+++ packages/ckeditor5-dev-build-tools/src/utils.ts
@@ -1,8 +1,27 @@
 import path from 'upath';
+import type { CamelCase, CamelCasedProperties } from 'type-fest';
 
 /**
  * Returns path relative to the current working directory.
  */
 export function getPath( ...paths: string[] ): string {
 	return path.join( process.cwd(), ...paths );
+}
+
+/**
+ * Transforms `kebab-case` strings to `camelCase`.
+ */
+export function camelize<const T extends string>( s: T ): CamelCase<T> {
+	return s.replace( /-./g, x => x[1]!.toUpperCase() ) as CamelCase<T>;
+}
+
+/**
+ * Transforms all object keys from `kebab-case` to `camelCase`.
+ */
+export function camelizeObjectKeys<const T extends Record<string, any>>( obj: T ): CamelCasedProperties<T> {
+	return Object.fromEntries(
+		Object
+			.entries( obj )
+			.map( ( [ key, value ] ) => [ camelize( key ), value ] )
+	) as CamelCasedProperties<T>;
 }
