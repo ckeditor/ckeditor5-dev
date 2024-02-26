04782c872a9a3156516a9a2e1d28b901aada3ed4
Piotr Szczęśniak <szczesniakp@o2.pl>
2024-02-26 10:42:50 +0100

    Fix typo in test name.

--- packages/ckeditor5-dev-build-tools/tests/plugins/splitCss/splitCss.test.ts
+++ packages/ckeditor5-dev-build-tools/tests/plugins/splitCss/splitCss.test.ts
@@ -155,7 +155,7 @@ describe( 'splitCss', () => {
 		verifyDividedStyleSheet( output, 'content-styles.css', expectedContentResult );
 	} );
 
-	test( 'should not duplicate declaration ins `:root`', async () => {
+	test( 'should not duplicate declaration in `:root` selector', async () => {
 		const output = await generateBundle( './fixtures/duplicated-definitions/input.ts' );
 
 		const expectedResult = removeWhitespace(
