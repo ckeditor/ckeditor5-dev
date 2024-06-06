62ab5d6014ceca0ff66ffb0469a60718ff74952d
Marta Motyczynska <m.motyczynska@cksource.com>
2024-06-06 14:07:07 +0200

    Pass 'GPL' as global license key in tests (automated & manual).

--- packages/ckeditor5-dev-tests/lib/utils/manual-tests/globallicensekey.js
+++ packages/ckeditor5-dev-tests/lib/utils/manual-tests/globallicensekey.js
@@ -5,7 +5,4 @@
 
 /* global window */
 
-// eslint-disable-next-line mocha/no-top-level-hooks
-before( function() {
-	window.CKEDITOR_IS_TEST_ENV = true;
-} );
+window.CKEDITOR_GLOBAL_LICENSE_KEY = 'GPL';
