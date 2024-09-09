2690820a2085a262ce90a7925a6ccc75757c73a2
Paweł Smyrek <p.smyrek@cksource.com>
2024-09-09 12:29:16 +0200

    Aligned tests in `utils/getnewversiontype.js`.

--- packages/ckeditor5-dev-release-tools/tests/utils/getnewversiontype.js
+++ packages/ckeditor5-dev-release-tools/tests/utils/getnewversiontype.js
@@ -3,82 +3,80 @@
  * For licensing, see LICENSE.md.
  */
 
-'use strict';
-
-const expect = require( 'chai' ).expect;
-const getNewVersionType = require( '../../lib/utils/getnewversiontype' );
+import { describe, it, expect } from 'vitest';
+import { getNewVersionType } from '../../lib/utils/getnewversiontype.js';
 
 describe( 'dev-release-tools/utils', () => {
 	describe( 'getSubPackagesPaths()', () => {
 		it( 'returns "skip" when passing an empty array of commits', () => {
-			expect( getNewVersionType( [] ) ).to.equal( 'skip' );
+			expect( getNewVersionType( [] ) ).toEqual( 'skip' );
 		} );
 
 		it( 'returns "internal" when passing non-public commits', () => {
-			expect( getNewVersionType( [ { isPublicCommit: false } ] ) ).to.equal( 'internal' );
+			expect( getNewVersionType( [ { isPublicCommit: false } ] ) ).toEqual( 'internal' );
 		} );
 
 		it( 'returns "major" if MAJOR BREAKING CHANGES was introduced in "type:Other" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'MAJOR BREAKING CHANGES' } ], rawType: 'Other' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'major' );
+			expect( getNewVersionType( commits ) ).toEqual( 'major' );
 		} );
 
 		it( 'returns "major" if BREAKING CHANGES was introduced in "type:Other" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'BREAKING CHANGES' } ], rawType: 'Other' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'major' );
+			expect( getNewVersionType( commits ) ).toEqual( 'major' );
 		} );
 
 		it( 'returns "major" if MAJOR BREAKING CHANGES was introduced in "type:Fix" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'MAJOR BREAKING CHANGES' } ], rawType: 'Fix' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'major' );
+			expect( getNewVersionType( commits ) ).toEqual( 'major' );
 		} );
 
 		it( 'returns "major" if BREAKING CHANGES was introduced in "type:Fix" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'BREAKING CHANGES' } ], rawType: 'Fix' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'major' );
+			expect( getNewVersionType( commits ) ).toEqual( 'major' );
 		} );
 
 		it( 'returns "major" if MAJOR BREAKING CHANGES was introduced in "type:Feature" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'MAJOR BREAKING CHANGES' } ], rawType: 'Feature' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'major' );
+			expect( getNewVersionType( commits ) ).toEqual( 'major' );
 		} );
 
 		it( 'returns "major" if BREAKING CHANGES was introduced in "type:Feature" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'BREAKING CHANGES' } ], rawType: 'Feature' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'major' );
+			expect( getNewVersionType( commits ) ).toEqual( 'major' );
 		} );
 
 		it( 'returns "minor" if MINOR BREAKING CHANGES was introduced in "type:Other" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'MINOR BREAKING CHANGES' } ], rawType: 'Other' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'minor' );
+			expect( getNewVersionType( commits ) ).toEqual( 'minor' );
 		} );
 
 		it( 'returns "minor" if MINOR BREAKING CHANGES was introduced in "type:Fix" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'MINOR BREAKING CHANGES' } ], rawType: 'Fix' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'minor' );
+			expect( getNewVersionType( commits ) ).toEqual( 'minor' );
 		} );
 
 		it( 'returns "minor" if MINOR BREAKING CHANGES was introduced in "type:Feature" commit', () => {
 			const commits = [
 				{ isPublicCommit: true, notes: [ { title: 'MINOR BREAKING CHANGES' } ], rawType: 'Feature' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'minor' );
+			expect( getNewVersionType( commits ) ).toEqual( 'minor' );
 		} );
 
 		it( 'returns "minor" if found "type:Feature" commit in the collection', () => {
@@ -88,7 +86,7 @@ describe( 'dev-release-tools/utils', () => {
 				{ isPublicCommit: false, notes: [], rawType: 'Docs' },
 				{ isPublicCommit: true, notes: [], rawType: 'Feature' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'minor' );
+			expect( getNewVersionType( commits ) ).toEqual( 'minor' );
 		} );
 
 		it( 'returns "major" if found "MAJOR BREAKING CHANGES" commit in the collection', () => {
@@ -105,7 +103,7 @@ describe( 'dev-release-tools/utils', () => {
 					rawType: 'Feature'
 				}
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'major' );
+			expect( getNewVersionType( commits ) ).toEqual( 'major' );
 		} );
 
 		it( 'returns "patch" if no breaking changes or features commits were made', () => {
@@ -113,7 +111,7 @@ describe( 'dev-release-tools/utils', () => {
 				{ isPublicCommit: true, notes: [], rawType: 'Fix' },
 				{ isPublicCommit: true, notes: [], rawType: 'Other' }
 			];
-			expect( getNewVersionType( commits ) ).to.equal( 'patch' );
+			expect( getNewVersionType( commits ) ).toEqual( 'patch' );
 		} );
 	} );
 } );
