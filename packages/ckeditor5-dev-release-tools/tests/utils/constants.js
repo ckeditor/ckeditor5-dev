74880b09b4749d9eeaaf2e25d04af4a0de312918
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-09-10 08:53:28 +0200

    More tests.

---
+++ packages/ckeditor5-dev-release-tools/tests/utils/constants.js
@@ -0,0 +1,22 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import { describe, expect, it } from 'vitest';
+import * as constants from '../../lib/utils/constants.js';
+
+describe( 'constants', () => {
+	it( '#CHANGELOG_FILE', async () => {
+		expect( constants.CHANGELOG_FILE ).to.be.a( 'string' );
+	} );
+
+	it( '#CHANGELOG_HEADER', async () => {
+		expect( constants.CHANGELOG_HEADER ).to.be.a( 'string' );
+	} );
+} );
