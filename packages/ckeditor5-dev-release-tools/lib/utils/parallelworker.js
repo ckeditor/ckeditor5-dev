eea371744b62113419b7ad680cf5a1959becb140
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-09-11 08:58:33 +0200

    Replaced an integration test with a unit one for a worker script.

--- packages/ckeditor5-dev-release-tools/lib/utils/parallelworker.js
+++ packages/ckeditor5-dev-release-tools/lib/utils/parallelworker.js
@@ -5,13 +5,14 @@
 
 // This file is covered by the "executeInParallel() - integration" test cases.
 
+import { parentPort, workerData } from 'worker_threads';
+
 // Required due to top-level await.
 ( async () => {
 	/**
 	 * @param {String} callbackModule
 	 * @param {Array.<String>} packages
 	 */
-	const { parentPort, workerData } = await import( 'worker_threads' );
 	const { default: callback } = await import( workerData.callbackModule );
 
 	for ( const packagePath of workerData.packages ) {
--- packages/ckeditor5-dev-release-tools/vitest.config.js
+++ packages/ckeditor5-dev-release-tools/vitest.config.js
@@ -6,6 +6,9 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig( {
+	plugins: [
+		executeInParallelVirtualModulePlugin()
+	],
 	test: {
 		setupFiles: [
 			'./tests/_utils/testsetup.js'
@@ -29,3 +32,22 @@ export default defineConfig( {
 		}
 	}
 } );
+
+function executeInParallelVirtualModulePlugin() {
+	const virtualModuleId = 'virtual:parallelworker-integration-module';
+	const resolvedVirtualModuleId = '\0' + virtualModuleId;
+
+	return {
+		name: 'execute-in-parallel-virtual-module-plugin',
+		resolveId( id ) {
+			if ( id === virtualModuleId ) {
+				return resolvedVirtualModuleId;
+			}
+		},
+		load( id ) {
+			if ( id === resolvedVirtualModuleId ) {
+				return 'export default function virtualModule() {}';
+			}
+		}
+	};
+}
