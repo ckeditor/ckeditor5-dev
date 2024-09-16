9563123e27b1b9ed323b44ab7a0c4cf072878b7f
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-09-16 13:18:24 +0200

    First tests.

---
+++ packages/ckeditor5-dev-tests/tests/index.js
@@ -0,0 +1,37 @@
+/**
+ * @license Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import { describe, expect, it, vi } from 'vitest';
+import * as index from '../lib/index.js';
+import runAutomatedTests from '../lib/tasks/runautomatedtests.js';
+import runManualTests from '../lib/tasks/runmanualtests.js';
+import parseArguments from '../lib/utils/automated-tests/parsearguments.js';
+
+vi.mock( '../lib/tasks/runautomatedtests.js' );
+vi.mock( '../lib/tasks/runmanualtests.js' );
+vi.mock( '../lib/utils/automated-tests/parsearguments.js' );
+
+describe( 'index.js', () => {
+	describe( 'runAutomatedTests()', () => {
+		it( 'should be a function', () => {
+			expect( index.runAutomatedTests ).to.be.a( 'function' );
+			expect( index.runAutomatedTests ).toEqual( runAutomatedTests );
+		} );
+	} );
+
+	describe( 'runManualTests()', () => {
+		it( 'should be a function', () => {
+			expect( index.runManualTests ).to.be.a( 'function' );
+			expect( index.runManualTests ).toEqual( runManualTests );
+		} );
+	} );
+
+	describe( 'parseArguments()', () => {
+		it( 'should be a function', () => {
+			expect( index.parseArguments ).to.be.a( 'function' );
+			expect( index.parseArguments ).toEqual( parseArguments );
+		} );
+	} );
+} );
