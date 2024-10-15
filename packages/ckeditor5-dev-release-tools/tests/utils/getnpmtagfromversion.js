4edf71cade5c73a8f9a7dbf80994490eeb400b60
Kamil Piechaczek <pomek@users.noreply.github.com>
2024-10-15 15:06:15 +0200

    getNpmTagFromVersion() supports a nightly version.

--- packages/ckeditor5-dev-release-tools/tests/utils/getnpmtagfromversion.js
+++ packages/ckeditor5-dev-release-tools/tests/utils/getnpmtagfromversion.js
@@ -24,4 +24,10 @@ describe( 'getNpmTagFromVersion()', () => {
 		expect( getNpmTagFromVersion( '2.1.0-alpha.0' ) ).to.equal( 'alpha' );
 		expect( getNpmTagFromVersion( '3.2.1-alpha.0' ) ).to.equal( 'alpha' );
 	} );
+
+	it( 'should return "nightly" when processing a 0.0.0-nightly-YYYYMMDD.X version', () => {
+		vi.mocked( semver.prerelease ).mockReturnValue( [ 'nightly', 0 ] );
+
+		expect( getNpmTagFromVersion( '0.0.0-nightly-20230517.0' ) ).to.equal( 'nightly' );
+	} );
 } );
