69714259690e8470b7e5ed90c9de64f4a556a421
Marcin Panek <m.panek@cksource.com>
2025-03-21 10:09:56 +0100

    Generating a changelog for a monorepository [WIP].

--- packages/ckeditor5-dev-changelog/tests/generateChangelog.test.ts
+++ packages/ckeditor5-dev-changelog/tests/generateChangelog.test.ts
@@ -3,9 +3,9 @@
  * For licensing, see LICENSE.md.
  */
 
-import { generateChangelog } from '../src/index.js';
+import { generatechangelog } from '../src/index.js';
 import { test, expect } from 'vitest';
 
 test( 'returns the expected string', () => {
-	expect( generateChangelog() ).toEqual( 'Hello from ckeditor5-dev-changelog!' );
+	expect( generatechangelog() ).toEqual( 'Hello from ckeditor5-dev-changelog!' );
 } );
