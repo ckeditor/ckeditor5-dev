92fd475582aca246a7b6a62228762e15673c7909
Paweł Smyrek <p.smyrek@cksource.com>
2025-04-11 13:52:37 +0200

    Adjusted `interface-augmentation-fixer` plugin to TypeDoc@0.28.x.

--- packages/typedoc-plugins/lib/index.ts
+++ packages/typedoc-plugins/lib/index.ts
@@ -14,3 +14,4 @@ export { default as typeDocTagEvent } from './tag-event/index.js';
 export { default as typeDocTagObservable } from './tag-observable/index.js';
 export { default as typeDocEventParamFixer } from './event-param-fixer/index.js';
 export { default as typeDocEventInheritanceFixer } from './event-inheritance-fixer/index.js';
+export { default as typeDocInterfaceAugmentationFixer } from './interface-augmentation-fixer/index.js';
