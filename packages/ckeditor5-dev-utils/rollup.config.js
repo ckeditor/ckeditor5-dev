623b7de3d9b8607d8d5addb2f1fcc856aee4cf79
Kamil Piechaczek <pomek@users.noreply.github.com>
2025-05-19 13:49:31 +0200

    TypeScript init.

---
+++ packages/ckeditor5-dev-utils/rollup.config.js
@@ -0,0 +1,41 @@
+t/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import { readFileSync } from 'fs';
+import path from 'upath';
+import { defineConfig } from 'rollup';
+import typescript from '@rollup/plugin-typescript';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+// Current working directory
+const cwd = process.cwd();
+
+// Content of the `package.json`
+const pkg = JSON.parse(
+	readFileSync( path.join( cwd, 'package.json' ) )
+);
+
+// List of external dependencies
+const externals = [
+	...Object.keys( pkg.dependencies || {} ),
+	...Object.keys( pkg.peerDependencies || {} )
+];
+
+export default defineConfig( {
+	input: 'src/index.ts',
+	output: {
+		format: 'esm',
+		file: path.join( cwd, 'dist', 'index.js' ),
+		assetFileNames: '[name][extname]'
+	},
+	external: id => externals.some( name => id.startsWith( name ) ),
+	plugins: [
+		typescript(),
+		nodeResolve( {
+			extensions: [ '.mjs', '.js', '.json', '.node', '.ts', '.mts' ],
+			preferBuiltins: true
+		} )
+	]
+} );
---
+++ packages/ckeditor5-dev-utils/vitest.config.ts
@@ -0,0 +1,26 @@
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig( {
+	test: {
+		testTimeout: 10000,
+		restoreMocks: true,
+		coverage: {
+			provider: 'v8',
+			include: [
+				'src/**'
+			],
+			exclude: [
+				'tests/**',
+				'src/types.ts',
+				'src/index.ts'
+			],
+			reporter: [ 'text', 'json', 'html' ],
+			all: true
+		}
+	}
+} );
