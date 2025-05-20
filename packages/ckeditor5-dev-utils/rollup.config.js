3323acca97f30dfe56084c4f3bf3c55a09a3c3d5
Kamil Piechaczek <pomek@users.noreply.github.com>
2025-05-20 14:11:06 +0200

    Cleanup.

--- packages/ckeditor5-dev-utils/rollup.config.js
+++ packages/ckeditor5-dev-utils/rollup.config.js
@@ -6,8 +6,6 @@
 import { readFileSync } from 'fs';
 import path from 'upath';
 import { defineConfig } from 'rollup';
-// import commonjs from '@rollup/plugin-commonjs';
-// import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
@@ -34,18 +32,6 @@ export default defineConfig( {
 	},
 	external: id => externals.some( name => id.startsWith( name ) ),
 	plugins: [
-		// /**
-		//  * Converts CommonJS modules to ES6.
-		//  */
-		// commonjs( {
-		// 	defaultIsModuleExports: true
-		// } ),
-		//
-		// /**
-		//  * Allows importing JSON files.
-		//  */
-		// json(),
-
 		typescript(),
 		nodeResolve( {
 			extensions: [ '.mjs', '.js', '.json', '.node', '.ts', '.mts' ],
