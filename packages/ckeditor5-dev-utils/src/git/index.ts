c5404c7332e339d08bb89004ad408c9044156e98
Kamil Piechaczek <pomek@users.noreply.github.com>
2025-05-22 14:43:20 +0200

    utils: added the "git" workspace that allows making a commit.

---
+++ packages/ckeditor5-dev-utils/src/git/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+export { default as commit } from './commit.js';
+
--- packages/ckeditor5-dev-utils/src/index.ts
+++ packages/ckeditor5-dev-utils/src/index.ts
@@ -6,6 +6,7 @@
 export { default as logger } from './logger/index.js';
 export * as builds from './builds/index.js';
 export * as bundler from './bundler/index.js';
+export * as git from './git/index.js';
 export * as loaders from './loaders/index.js';
 export * as stream from './stream/index.js';
 export * as styles from './styles/index.js';
