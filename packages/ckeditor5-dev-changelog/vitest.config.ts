e6b410bacb783daf348acd543f85a7c1d2deeeee
Marcin Panek <m.panek@cksource.com>
2025-05-27 16:24:55 +0200

    Added required 100% coverage in `ckediotr5-dev-changelog`.

--- packages/ckeditor5-dev-changelog/vitest.config.ts
+++ packages/ckeditor5-dev-changelog/vitest.config.ts
@@ -19,6 +19,12 @@ export default defineConfig( {
 				'src/types.ts',
 				'src/index.ts'
 			],
+			thresholds: {
+				lines: 100,
+				functions: 100,
+				branches: 100,
+				statements: 100
+			},
 			reporter: [ 'text', 'json', 'html' ],
 			all: true
 		}
