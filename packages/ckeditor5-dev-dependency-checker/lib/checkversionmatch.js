c076a394a9508b0b183e0b6cc9648a44c57d604a
Przemysław Zań <przemyslaw.zan@gmail.com>
2025-05-30 16:36:09 +0200

    Added check versions script.

---
+++ packages/ckeditor5-dev-dependency-checker/lib/checkversionmatch.js
@@ -0,0 +1,230 @@
+#!/usr/bin/env node
+
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import fs from 'fs-extra';
+import chalk from 'chalk';
+import semver from 'semver';
+import { globSync } from 'glob';
+import { execSync } from 'child_process';
+
+/**
+ * This script ensures that all "dependencies" in package JSONs use the same versions of dependencies.
+ * It also checks that all versions are pinned, and they don't use the caret operator "^".
+ *
+ * @param {Object} options
+ * @param {string} options.cwd
+ * @param {boolean} [options.fix=false] Whether the script should automatically fix the errors.
+ * @param {Function} [options.isCkeditor5Package]
+ * @param {Object} [options.versionExceptions]
+ */
+export default async function checkVersionMatch( {
+	cwd,
+	fix = false,
+	isCkeditor5Package = () => false,
+	versionExceptions = {}
+} ) {
+	console.log( chalk.blue( '🔍 Starting checking dependencies versions...' ) );
+
+	const versionsCache = {};
+
+	const [ packageJsons, pathMappings ] = getPackageJsons( { cwd, directories: [
+		'package.json',
+		'packages/*/package.json',
+		'external/ckeditor5-commercial/packages/*/package.json',
+		'external/ckeditor5-commercial/package.json'
+	] } );
+
+	const expectedDependencies = getExpectedDepsVersions( { packageJsons, isCkeditor5Package, versionExceptions, versionsCache } );
+
+	if ( fix ) {
+		fixDependenciesVersions( { expectedDependencies, packageJsons, pathMappings, isCkeditor5Package } );
+	} else {
+		checkDependenciesMatch( { expectedDependencies, packageJsons, isCkeditor5Package } );
+	}
+}
+
+/**
+ * @param {Object} options
+ * @param {Object.<String, String>} options.expectedDependencies
+ * @param {Array.<Object>} options.packageJsons
+ * @param {Object.<String, String>} options.pathMappings
+ * @param {Function} options.isCkeditor5Package
+ */
+function fixDependenciesVersions( { expectedDependencies, packageJsons, pathMappings, isCkeditor5Package } ) {
+	packageJsons
+		.filter( packageJson => packageJson.dependencies )
+		.forEach( packageJson => {
+			for ( const [ dependency, version ] of Object.entries( packageJson.dependencies ) ) {
+				if ( version === expectedDependencies[ dependency ] ) {
+					continue;
+				}
+
+				packageJson.dependencies[ dependency ] = expectedDependencies[ dependency ];
+			}
+
+			for ( const [ dependency, version ] of Object.entries( packageJson.devDependencies ) ) {
+				if ( !isCkeditor5Package( dependency ) || version === expectedDependencies[ dependency ] ) {
+					continue;
+				}
+
+				packageJson.devDependencies[ dependency ] = expectedDependencies[ dependency ];
+			}
+
+			fs.writeJsonSync( pathMappings[ packageJson.name ], packageJson, { spaces: 2 } );
+		} );
+
+	console.log( chalk.green( '✅  All dependencies fixed!' ) );
+}
+
+/**
+ * @param {Object} options
+ * @param {Object.<String, String>} options.expectedDependencies
+ * @param {Function} options.isCkeditor5Package
+ * @param {Array.<Object>} options.packageJsons
+ */
+function checkDependenciesMatch( { expectedDependencies, packageJsons, isCkeditor5Package } ) {
+	const errors = packageJsons
+		.flatMap( packageJson => {
+			const depsErrors = Object.entries( packageJson.dependencies || {} )
+				.map( ( [ dependency, version ] ) => {
+					if ( version === expectedDependencies[ dependency ] ) {
+						return '';
+					}
+
+					return getWrongVersionErrorMsg( { dependency, name: packageJson.name, version, expectedDependencies } );
+				} )
+				.filter( Boolean );
+
+			const devDepsErrors = Object.entries( packageJson.devDependencies || {} )
+				.map( ( [ dependency, version ] ) => {
+					if ( !isCkeditor5Package( dependency ) || version === expectedDependencies[ dependency ] ) {
+						return '';
+					}
+
+					return getWrongVersionErrorMsg( { dependency, name: packageJson.name, version, expectedDependencies } );
+				} )
+				.filter( Boolean );
+
+			return [ ...depsErrors, devDepsErrors ].flat();
+		} );
+
+	if ( errors.length ) {
+		console.error( chalk.red( '❌  Errors found. Run this script with an argument: `--fix` to resolve the issues automatically:' ) );
+		console.error( chalk.red( errors.join( '\n' ) ) );
+		process.exit( 1 );
+	}
+
+	console.log( chalk.green( '✅  All dependencies are correct!' ) );
+}
+
+/**
+ * @param {Object} options
+ * @param {String} options.dependency
+ * @param {String} options.name
+ * @param {String} options.version
+ * @param {Object.<String, String>} options.expectedDependencies
+ */
+function getWrongVersionErrorMsg( { dependency, name, version, expectedDependencies } ) {
+	return `"${ dependency }" in "${ name }" in version "${ version }" should be set to "${ expectedDependencies[ dependency ] }".`;
+}
+
+/**
+ * @param {Object} options
+ * @param {Array.<Object>} options.packageJsons
+ * @param {Function} options.isCkeditor5Package
+ * @param {Object} options.versionExceptions
+ * @param {Object} options.versionsCache
+ * @return {Object.<String, String>} expectedDependencies
+ */
+function getExpectedDepsVersions( { packageJsons, isCkeditor5Package, versionExceptions, versionsCache } ) {
+	return packageJsons
+		.reduce( ( expectedDependencies, packageJson ) => {
+			for ( const [ dependency, version ] of Object.entries( packageJson.dependencies || {} ) ) {
+				expectedDependencies[ dependency ] = getNewestVersion( {
+					packageName: dependency,
+					newVersion: version,
+					versionsCache,
+					versionExceptions,
+					currentMaxVersion: expectedDependencies[ dependency ]
+				} );
+			}
+
+			for ( const [ dependency, version ] of Object.entries( packageJson.devDependencies || {} ) ) {
+				if ( !isCkeditor5Package( dependency ) ) {
+					continue;
+				}
+
+				expectedDependencies[ dependency ] = getNewestVersion( {
+					packageName: dependency,
+					newVersion: version,
+					versionsCache,
+					versionExceptions,
+					currentMaxVersion: expectedDependencies[ dependency ]
+				} );
+			}
+
+			return expectedDependencies;
+		}, {} );
+}
+
+/**
+ * @param {Object} options
+ * @param {String} options.packageName
+ * @param {Object} options.versionsCache
+ * @param {Object} options.versionExceptions
+ * @param {String} [options.newVersion='0.0.0']
+ * @param {String} [options.currentMaxVersion='0.0.0']
+ * @return {String}
+ */
+function getNewestVersion( {
+	packageName,
+	versionsCache,
+	versionExceptions,
+	newVersion = '0.0.0',
+	currentMaxVersion = '0.0.0'
+} ) {
+	if ( versionExceptions[ packageName ] ) {
+		return newVersion;
+	}
+
+	const newMaxVersion = semver.valid( newVersion ) ?
+		newVersion :
+		semver.maxSatisfying( getVersionsList( { packageName, versionsCache } ), newVersion );
+
+	return semver.gt( newMaxVersion, currentMaxVersion ) ? newMaxVersion : currentMaxVersion;
+}
+
+/**
+ * @param {Object} options
+ * @param {String} options.packageName
+ * @param {Object} options.versionsCache
+ * @return {Object.<String, String>}
+ */
+function getVersionsList( { packageName, versionsCache } ) {
+	if ( !versionsCache[ packageName ] ) {
+		console.log( chalk.blue( `⬇️ Downloading "${ packageName }" versions from npm...` ) );
+		const versionsJson = execSync( `npm view ${ packageName } versions --json`, { encoding: 'utf8' } );
+		versionsCache[ packageName ] = JSON.parse( versionsJson );
+	}
+
+	return versionsCache[ packageName ];
+}
+
+/**
+ * @param {Object} options
+ * @param {String} options.cwd
+ * @param {Array.<String>} options.directories
+ * @return {[Array.<Object>, Object.<String, String>]}
+ */
+function getPackageJsons( { cwd, directories } ) {
+	const packageJsonPaths = globSync( directories, { absolute: true, cwd } );
+	const packageJsons = packageJsonPaths.map( packageJsonPath => fs.readJsonSync( packageJsonPath ) );
+	const nameToPathMappings = packageJsonPaths
+		.reduce( ( accum, packageJsonPath ) => ( { ...accum, [ fs.readJsonSync( packageJsonPath ).name ]: packageJsonPath } ), {} );
+
+	return [ packageJsons, nameToPathMappings ];
+}
---
+++ scripts/ci/check-dependencies-versions-match.mjs
@@ -0,0 +1,11 @@
+/**
+ * @license Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+import upath from 'upath';
+import { checkVersionMatch } from '../../packages/ckeditor5-dev-dependency-checker/lib/index.js';
+
+const ROOT_DIRECTORY = upath.join( import.meta.dirname, '..', '..' );
+
+checkVersionMatch( { cwd: ROOT_DIRECTORY } );
