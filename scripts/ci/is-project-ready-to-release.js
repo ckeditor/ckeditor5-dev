6c219705bcca34eea6a9e36f809875c072f77089
Kamil Piechaczek <pomek@users.noreply.github.com>
2025-12-15 10:36:39 +0100

    Revert "Switch to npm "next" tag when releasing packages."
    This reverts commit 1fb58ee57ee6c8612712a305eb9a0652bac9af5b.

--- scripts/ci/is-project-ready-to-release.js
+++ scripts/ci/is-project-ready-to-release.js
@@ -10,11 +10,7 @@ import * as releaseTools from '@ckeditor/ckeditor5-dev-release-tools';
 const changelogVersion = releaseTools.getLastFromChangelog();
 const npmTag = releaseTools.getNpmTagFromVersion( changelogVersion );
 
-// As long as CKEditor 5 supports the old installation methods, to avoid breaking the existing configurations,
-// packages from `ckeditor5-dev` are released as `@next`.
-const temporaryTagToCheck = npmTag === 'latest' ? 'next' : npmTag;
-
-releaseTools.isVersionPublishableForTag( '@ckeditor/ckeditor5-dev-release-tools', changelogVersion, temporaryTagToCheck )
+releaseTools.isVersionPublishableForTag( '@ckeditor/ckeditor5-dev-release-tools', changelogVersion, npmTag )
 	.then( result => {
 		if ( !result ) {
 			console.error( `The proposed changelog (${ changelogVersion }) version is not higher than the already published one.` );
