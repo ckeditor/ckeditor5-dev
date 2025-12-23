d1deb6e711b06ae4995d331f6086d2ce9f2189cc
Paweł Smyrek <p.smyrek@cksource.com>
2025-12-23 13:55:23 +0100

    Automatically normalize the `see` and `closes` fields from changelog entry into arrays when they are literal values.

--- packages/ckeditor5-dev-changelog/src/utils/normalizeentry.ts
+++ packages/ckeditor5-dev-changelog/src/utils/normalizeentry.ts
@@ -67,14 +67,10 @@ function deduplicate( packageNames: Array<string> | undefined ): Array<string> {
 	return [ ...new Set( packageNames ) ];
 }
 
-function toArray( input: string | Array<string> | undefined ): Array<string> {
+function toArray( input: unknown ): Array<unknown> {
 	if ( !input ) {
 		return [];
 	}
 
-	if ( typeof input === 'string' ) {
-		return [ input ];
-	}
-
-	return input;
+	return Array.isArray( input ) ? input : [ input ];
 }
