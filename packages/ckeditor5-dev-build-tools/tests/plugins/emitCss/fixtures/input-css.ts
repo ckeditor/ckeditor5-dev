e8255375adec33f4d183c970ca1c6d321cde0303
Filip Sobol <filipsobol@users.noreply.github.com>
2026-05-27 16:16:50 +0200

    Remove unused fixtures.

--- packages/ckeditor5-dev-build-tools/tests/plugins/emitCss/fixtures/input-css.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license Copyright (c) 2003-2026, CKSource Holding sp. z o.o. All rights reserved.
- * For licensing, see LICENSE.md.
- */
-
-import './input.css';
-
-export const test = 123;
